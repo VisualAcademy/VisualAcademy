3efeffe1b713490f6f1930d1ca52a6bcc21c579c
Park YongJune <redplus@redplus.net>
2022-11-27 16:40:43 +0900

    Swagger UI

--- src/VisualAcademy/VisualAcademy.Todos/Program.cs
+++ src/VisualAcademy/VisualAcademy.Todos/Program.cs
@@ -6,7 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(
     opt => opt.UseInMemoryDatabase("Todos"));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
