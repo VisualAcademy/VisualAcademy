39465b57e61d3e2a4cd72291246434fb6b4ebebf
Park YongJune <redplus@redplus.net>
2022-11-27 12:48:29 +0900

    파일 범위 네임스페이스로 변환

--- src/VisualAcademy/VisualAcademy.Models.Notes/01_02_INoteRepository.cs
+++ src/VisualAcademy/VisualAcademy.Models.Notes/01_02_INoteRepository.cs
@@ -1,32 +1,47 @@
-﻿namespace Hawaso.Models.Notes
+﻿namespace Hawaso.Models.Notes;
+
+public interface INoteRepository
 {
-    public interface INoteRepository
-    {
-        // 게시판에서 사용
-        void Add(Note model);
-        int DeleteNote(int id, string password);
-        List<Note>? GetAll(int page);
-        int GetCountAll();
-        int GetCountBySearch(string searchField, string searchQuery);
-        string? GetFileNameById(int id);
-        Note? GetNoteById(int id);
-        List<Note> GetSeachAll(
-            int page, string searchField, string searchQuery);
-        void ReplyNote(Note model);
-        void UpdateDownCount(string fileName);
-        void UpdateDownCountById(int id);
-        int UpdateNote(Note model);
+    // 게시판에서 사용
+    void Add(Note model);
 
-        // 메인 페이지에서 사용
-        List<Note> GetRecentPosts();
-        List<Note>? GetRecentPostsCache();
-        List<Note> GetNewPhotos();
-        List<Note>? GetNewPhotosCache();
-        List<Note> GetNoteSummaryByCategory(string category);
-        List<Note>? GetNoteSummaryByCategoryCache(string category);
-        List<Note> GetNoteSummaryByCategoryBlog(string category);
+    int DeleteNote(int id, string password);
 
-        // 관리자 기능(페이지)에서 사용
-        void Pinned(int id);
-    }
+    List<Note>? GetAll(int page);
+
+    int GetCountAll();
+
+    int GetCountBySearch(string searchField, string searchQuery);
+
+    string? GetFileNameById(int id);
+
+    Note? GetNoteById(int id);
+
+    List<Note> GetSeachAll(int page, string searchField, string searchQuery);
+
+    void ReplyNote(Note model);
+
+    void UpdateDownCount(string fileName);
+
+    void UpdateDownCountById(int id);
+
+    int UpdateNote(Note model);
+
+    // 메인 페이지에서 사용
+    List<Note> GetRecentPosts();
+
+    List<Note>? GetRecentPostsCache();
+
+    List<Note> GetNewPhotos();
+
+    List<Note>? GetNewPhotosCache();
+
+    List<Note> GetNoteSummaryByCategory(string category);
+
+    List<Note>? GetNoteSummaryByCategoryCache(string category);
+
+    List<Note> GetNoteSummaryByCategoryBlog(string category);
+
+    // 관리자 기능(페이지)에서 사용
+    void Pinned(int id);
 }
