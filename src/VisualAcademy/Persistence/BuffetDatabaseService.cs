71c8ff70da9f631ce118bf56b49c4efe53dade36
Park YongJune <redplus@redplus.net>
2022-12-24 01:58:50 +0900

    BuffetDatabaseService Web API GET, POST tests

--- src/VisualAcademy/Persistence/BuffetDatabaseService.cs
+++ src/VisualAcademy/Persistence/BuffetDatabaseService.cs
@@ -1,16 +1,29 @@
 ﻿using Application.Common.Interfaces.Buffets;
 using Domain.Entities.Buffets;
 using Microsoft.EntityFrameworkCore;
+using Persistence.Features.Buffets;
 
 namespace Persistence
 {
     public class BuffetDatabaseService : DbContext, IBuffetDatabaseService
     {
+        public BuffetDatabaseService(DbContextOptions<BuffetDatabaseService> options) : base(options)
+        {
+            Database.EnsureCreated(); // 인-메모리 데이터베이스 생성
+        }
+
         public DbSet<Broth> Broths { get; set; }
 
         public void Save()
         {
             this.SaveChanges();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            new BrothConfiguration().Configure(modelBuilder.Entity<Broth>()); 
         }
     }
 }
--- src/VisualAcademy/Service/Program.cs
+++ src/VisualAcademy/Service/Program.cs
@@ -1,6 +1,22 @@
+using Domain.Entities.Buffets;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BuffetDatabaseService>(opt => opt.UseInMemoryDatabase("Buffets"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/broths", async (BuffetDatabaseService db) => await db.Broths.ToListAsync());
+
+app.MapPost("/broths", async (Broth broth, BuffetDatabaseService db) =>
+{
+    db.Broths.Add(broth);
+    await db.SaveChangesAsync();
+    return TypedResults.Ok();
+});
 
 app.Run();
