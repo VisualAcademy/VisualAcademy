911e7730fd986ac0280f9103d3ada350ad926ac7
Park YongJune <redplus@redplus.net>
2022-12-24 01:17:25 +0900

    BrothConfiguration

---
+++ src/VisualAcademy/Persistence/Features/Buffets/BrothConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.Buffets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Features.Buffets
+{
+    public class BrothConfiguration : IEntityTypeConfiguration<Broth>
+    {
+        public void Configure(EntityTypeBuilder<Broth> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(b => b.Name).IsRequired().HasMaxLength(25);
+
+            builder.HasData(
+                new Broth { Id = 1, Name = "콩국물", IsVegan = true },
+                new Broth { Id = 2, Name = "멸치국물", IsVegan = false });
+        }
+    }
+}
