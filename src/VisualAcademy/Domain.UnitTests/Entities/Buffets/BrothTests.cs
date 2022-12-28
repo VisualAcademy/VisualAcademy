071136aabd041df9258452c88da94c9f3f34fd3f
Park YongJune <redplus@redplus.net>
2022-12-28 18:10:42 +0900

    TodoTests.cs

--- src/VisualAcademy/Domain.UnitTests/Entities/Buffets/BrothTests.cs
+++ src/VisualAcademy/Domain.UnitTests/Entities/Buffets/BrothTests.cs
@@ -1,37 +1,36 @@
 ﻿using Domain.Entities.Buffets;
 using NUnit.Framework;
 
-namespace Domain.UnitTests.Entities.Buffets
+namespace Domain.UnitTests.Entities.Buffets;
+
+[TestFixture]
+public class BrothTests
 {
-    [TestFixture]
-    public class BrothTests
+    private Broth _broth = null!;
+    private const int Id = 1;
+    private const string Name = "콩국물";
+
+    [SetUp]
+    public void SetUp()
     {
-        private Broth _broth = null!;
-        private const int Id = 1;
-        private const string Name = "콩국물";
+        _broth = new Broth();
+    }
 
-        [SetUp]
-        public void SetUp()
-        {
-            _broth = new Broth();
-        }
+    [Test]
+    public void TestSetAndGetId()
+    {
+        _broth.Id = Id;
 
-        [Test]
-        public void TestSetAndGetId()
-        {
-            _broth.Id = Id;
+        Assert.That(_broth.Id,
+            Is.EqualTo(Id));
+    }
 
-            Assert.That(_broth.Id,
-                Is.EqualTo(Id));
-        }
+    [Test]
+    public void TestSetAndGetName()
+    {
+        _broth.Name = Name;
 
-        [Test]
-        public void TestSetAndGetName()
-        {
-            _broth.Name = Name;
-
-            Assert.That(_broth.Name,
-                Is.EqualTo(Name));
-        }
+        Assert.That(_broth.Name,
+            Is.EqualTo(Name));
     }
 }
---
+++ src/VisualAcademy/Domain.UnitTests/Entities/TodoTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using NUnit.Framework;
+
+namespace Domain.UnitTests.Entities;
+
+[TestFixture]
+public class TodoTests
+{
+    private Todo _todo = null!;
+    private const int Id = 1;
+    private const string Name = "Test";
+
+    [SetUp]
+    public void SetUp()
+    {
+        _todo = new Todo();
+    }
+
+    [Test]
+    public void TestSetAndGetId()
+    {
+        _todo.Id = Id;
+
+        Assert.That(_todo.Id, Is.EqualTo(Id));
+    }
+
+    [Test]
+    public void TestSetAndGetName()
+    {
+        _todo.Name = Name;
+
+        Assert.That(_todo.Name, Is.EqualTo(Name));
+    }
+}
