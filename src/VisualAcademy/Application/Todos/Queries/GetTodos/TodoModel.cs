94266d57b0f8b36bdca611071db7685660ebba21
Park YongJune <redplus@redplus.net>
2022-12-31 18:59:29 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/Application/Todos/Queries/GetTodos/TodoModel.cs
+++ src/VisualAcademy/Application/Todos/Queries/GetTodos/TodoModel.cs
@@ -1,9 +1,8 @@
-﻿namespace Application.Todos.Queries.GetTodos
+﻿namespace Application.Todos.Queries.GetTodos;
+
+public class TodoModel
 {
-    public class TodoModel
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
 }
