9f1b31617e439cec081f7e09268256c448ddd84a
Park YongJune <redplus@redplus.net>
2022-12-31 20:20:19 +0900

    .

--- src/VisualAcademy/Application/Todos/Queries/GetTodo/GetTodoQuery.cs
+++ src/VisualAcademy/Application/Todos/Queries/GetTodo/GetTodoQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Todos.Queries.GetTodo
+﻿namespace Application.Todos.Queries.GetTodo
 {
     internal class GetTodoQuery
     {
