0579b4a4d3121df08e6cf4d559e30d68a94276d0
Park YongJune <redplus@redplus.net>
2023-01-03 20:18:09 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/Persistence/Todos/TodoConfiguration.cs
+++ src/VisualAcademy/Persistence/Todos/TodoConfiguration.cs
@@ -2,19 +2,18 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Persistence.Features.Buffets
+namespace Persistence.Features.Buffets;
+
+public class TodoConfiguration : IEntityTypeConfiguration<Todo>
 {
-    public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+    public void Configure(EntityTypeBuilder<Todo> builder)
     {
-        public void Configure(EntityTypeBuilder<Todo> builder)
-        {
-            builder.HasKey(x => x.Id);
+        builder.HasKey(x => x.Id);
 
-            builder.Property(b => b.Name).IsRequired().HasMaxLength(25);
+        builder.Property(b => b.Name).IsRequired().HasMaxLength(25);
 
-            builder.HasData(
-                new Todo { Id = 1, Name = "콩국물", IsComplete = true },
-                new Todo { Id = 2, Name = "멸치국물", IsComplete = false });
-        }
+        builder.HasData(
+            new Todo { Id = 1, Name = "콩국물", IsComplete = true },
+            new Todo { Id = 2, Name = "멸치국물", IsComplete = false });
     }
 }
