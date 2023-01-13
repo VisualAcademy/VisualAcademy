507c8aa91374683fb071eb8beaa49742752c31cd
Park YongJune <redplus@redplus.net>
2023-01-13 11:16:13 +0900

    Use expression body for constructors

--- src/VisualAcademy/VisualAcademy/Controllers/WeatherForecastController.cs
+++ src/VisualAcademy/VisualAcademy/Controllers/WeatherForecastController.cs
@@ -14,10 +14,7 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
+    public WeatherForecastController(ILogger<WeatherForecastController> logger) => _logger = logger;
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
