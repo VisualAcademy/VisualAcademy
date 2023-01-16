5b45e4975dfb2a677908f3b8548de1a733d95056
Park YongJune <redplus@redplus.net>
2023-01-16 12:08:14 +0900

    Use expression body for constructor

--- src/VisualAcademy/VisualAcademy/Pages/Index.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Index.cshtml.cs
@@ -7,10 +7,7 @@ namespace VisualAcademy.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        public IndexModel(ILogger<IndexModel> logger) => _logger = logger;
 
         public void OnGet()
         {
