00e56709738ef521418497b54cb1d2ce8f52799d
Park YongJune <redplus@redplus.net>
2023-01-17 05:50:28 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Areas/Identity/Models/ApplicationUser.cs
+++ src/VisualAcademy/VisualAcademy/Areas/Identity/Models/ApplicationUser.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace VisualAcademy.Areas.Identity.Models
+namespace VisualAcademy.Areas.Identity.Models;
+
+public class ApplicationUser : IdentityUser
 {
-    public class ApplicationUser : IdentityUser
-    {
-        public string? FirstName { get; set; }
+    public string? FirstName { get; set; }
 
-        public string? LastName { get; set; }
+    public string? LastName { get; set; }
 
-        public string? Timezone { get; set; }
+    public string? Timezone { get; set; }
 
-        // TODO: 
+    // TODO: 
 
-    }
 }
