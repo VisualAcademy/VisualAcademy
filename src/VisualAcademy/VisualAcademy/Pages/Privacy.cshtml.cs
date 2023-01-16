253f8e86113ec54b6b0f48e1b166a1e808158b88
Park YongJune <redplus@redplus.net>
2023-01-16 12:08:39 +0900

    Use expression body for constructor

--- src/VisualAcademy/VisualAcademy/Pages/Privacy.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Privacy.cshtml.cs
@@ -6,10 +6,7 @@ public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
-    {
-        _logger = logger;
-    }
+    public PrivacyModel(ILogger<PrivacyModel> logger) => _logger = logger;
 
     public void OnGet()
     {
