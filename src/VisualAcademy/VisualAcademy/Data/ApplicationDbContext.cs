d7767eca43df9e748f30e9ca816ac9e61c98ce53
Park YongJune <redplus@redplus.net>
2023-01-20 14:32:38 +0900

    SeedRoles

--- src/VisualAcademy/VisualAcademy/Data/ApplicationDbContext.cs
+++ src/VisualAcademy/VisualAcademy/Data/ApplicationDbContext.cs
@@ -11,6 +11,52 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Applicati
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        SeedRoles(builder);
+    }
+
+    private static void SeedRoles(ModelBuilder builder)
+    {
+        //[1] Groups(Roles)
+        //[1][1] ('Administrators', '관리자 그룹', 'Group', '응용 프로그램을 총 관리하는 관리 그룹 계정')
+        //[1][2] ('Everyone', '전체 사용자 그룹', 'Group', '응용 프로그램을 사용하는 모든 사용자 그룹 계정')
+        //[1][3] ('Users', '일반 사용자 그룹', 'Group', '일반 사용자 그룹 계정')
+        //[1][4] ('Guests', '게스트 그룹', 'Group', '게스트 사용자 그룹 계정')
+
+        builder.Entity<ApplicationRole>().HasData(
+            //[1][1] Administrators
+            new ApplicationRole()
+            {
+                Name = Dul.Roles.Administrators.ToString(),
+                NormalizedName = Dul.Roles.Administrators.ToString().ToUpper(),
+                Description = "응용 프로그램을 총 관리하는 관리 그룹 계정"
+            },
+            //[1][2] Everyone
+            new ApplicationRole()
+            {
+                Name = Dul.Roles.Everyone.ToString(),
+                NormalizedName = Dul.Roles.Everyone.ToString().ToUpper(),
+                Description = "응용 프로그램을 사용하는 모든 사용자 그룹 계정"
+            },
+            //[1][3] Users
+            new ApplicationRole()
+            {
+                Name = Dul.Roles.Users.ToString(),
+                NormalizedName = Dul.Roles.Users.ToString().ToUpper(),
+                Description = "일반 사용자 그룹 계정"
+            },
+            //[1][4] Guests
+            new ApplicationRole()
+            {
+                Name = Dul.Roles.Guests.ToString(),
+                NormalizedName = Dul.Roles.Guests.ToString().ToUpper(),
+                Description = "게스트 사용자 그룹 계정"
+            }
+        );
+    }
+
     public void SeedInitialData()
     { 
         // Empty
