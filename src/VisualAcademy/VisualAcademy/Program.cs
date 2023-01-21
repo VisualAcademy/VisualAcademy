25b1fa0ae540cb2b385745b42e79e0bf82a2e0a8
Park YongJune <redplus@redplus.net>
2023-01-21 20:41:45 +0900

    RequireDigit

--- src/VisualAcademy/VisualAcademy/Program.cs
+++ src/VisualAcademy/VisualAcademy/Program.cs
@@ -30,6 +30,8 @@ namespace VisualAcademy
                 {
                     options.SignIn.RequireConfirmedAccount = false;
                     options.SignIn.RequireConfirmedEmail = false;
+
+                    // options.Password.RequireDigit = false; 
                 })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
