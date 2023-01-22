407d4f54c94bfc769aaa7671a39e1088a6976b82
Park YongJune <redplus@redplus.net>
2023-01-22 19:58:55 +0900

    CreateBuiltInUsersAndRoles

--- src/VisualAcademy/VisualAcademy/Data/ApplicationDbContext.cs
+++ src/VisualAcademy/VisualAcademy/Data/ApplicationDbContext.cs
@@ -14,47 +14,51 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Applicati
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        SeedRoles(builder);
+        //SeedRoles(builder);
     }
 
+    /// <summary>
+    /// 기본 역할(Role)들을 생성하는 코드 중 하나 
+    /// </summary>
+    /// <param name="builder"></param>
     private static void SeedRoles(ModelBuilder builder)
     {
-        //[1] Groups(Roles)
-        //[1][1] ('Administrators', '관리자 그룹', 'Group', '응용 프로그램을 총 관리하는 관리 그룹 계정')
-        //[1][2] ('Everyone', '전체 사용자 그룹', 'Group', '응용 프로그램을 사용하는 모든 사용자 그룹 계정')
-        //[1][3] ('Users', '일반 사용자 그룹', 'Group', '일반 사용자 그룹 계정')
-        //[1][4] ('Guests', '게스트 그룹', 'Group', '게스트 사용자 그룹 계정')
+        ////[1] Groups(Roles)
+        ////[1][1] ('Administrators', '관리자 그룹', 'Group', '응용 프로그램을 총 관리하는 관리 그룹 계정')
+        ////[1][2] ('Everyone', '전체 사용자 그룹', 'Group', '응용 프로그램을 사용하는 모든 사용자 그룹 계정')
+        ////[1][3] ('Users', '일반 사용자 그룹', 'Group', '일반 사용자 그룹 계정')
+        ////[1][4] ('Guests', '게스트 그룹', 'Group', '게스트 사용자 그룹 계정')
 
-        builder.Entity<ApplicationRole>().HasData(
-            //[1][1] Administrators
-            new ApplicationRole()
-            {
-                Name = Dul.Roles.Administrators.ToString(),
-                NormalizedName = Dul.Roles.Administrators.ToString().ToUpper(),
-                Description = "응용 프로그램을 총 관리하는 관리 그룹 계정"
-            },
-            //[1][2] Everyone
-            new ApplicationRole()
-            {
-                Name = Dul.Roles.Everyone.ToString(),
-                NormalizedName = Dul.Roles.Everyone.ToString().ToUpper(),
-                Description = "응용 프로그램을 사용하는 모든 사용자 그룹 계정"
-            },
-            //[1][3] Users
-            new ApplicationRole()
-            {
-                Name = Dul.Roles.Users.ToString(),
-                NormalizedName = Dul.Roles.Users.ToString().ToUpper(),
-                Description = "일반 사용자 그룹 계정"
-            },
-            //[1][4] Guests
-            new ApplicationRole()
-            {
-                Name = Dul.Roles.Guests.ToString(),
-                NormalizedName = Dul.Roles.Guests.ToString().ToUpper(),
-                Description = "게스트 사용자 그룹 계정"
-            }
-        );
+        //builder.Entity<ApplicationRole>().HasData(
+        //    //[1][1] Administrators
+        //    new ApplicationRole()
+        //    {
+        //        Name = Dul.Roles.Administrators.ToString(),
+        //        NormalizedName = Dul.Roles.Administrators.ToString().ToUpper(),
+        //        Description = "응용 프로그램을 총 관리하는 관리 그룹 계정"
+        //    },
+        //    //[1][2] Everyone
+        //    new ApplicationRole()
+        //    {
+        //        Name = Dul.Roles.Everyone.ToString(),
+        //        NormalizedName = Dul.Roles.Everyone.ToString().ToUpper(),
+        //        Description = "응용 프로그램을 사용하는 모든 사용자 그룹 계정"
+        //    },
+        //    //[1][3] Users
+        //    new ApplicationRole()
+        //    {
+        //        Name = Dul.Roles.Users.ToString(),
+        //        NormalizedName = Dul.Roles.Users.ToString().ToUpper(),
+        //        Description = "일반 사용자 그룹 계정"
+        //    },
+        //    //[1][4] Guests
+        //    new ApplicationRole()
+        //    {
+        //        Name = Dul.Roles.Guests.ToString(),
+        //        NormalizedName = Dul.Roles.Guests.ToString().ToUpper(),
+        //        Description = "게스트 사용자 그룹 계정"
+        //    }
+        //);
     }
 
     public void SeedInitialData()
--- src/VisualAcademy/VisualAcademy/Program.cs
+++ src/VisualAcademy/VisualAcademy/Program.cs
@@ -11,7 +11,7 @@ namespace VisualAcademy
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -53,17 +53,17 @@ namespace VisualAcademy
             //[!] Configure...
 
             #region SeedInitialData
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var ctx = services.GetRequiredService<ApplicationDbContext>();
-                //ctx.Database.Migrate();
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+            //    var ctx = services.GetRequiredService<ApplicationDbContext>();
+            //    //ctx.Database.Migrate();
 
-                if (app.Environment.IsDevelopment())
-                {
-                    ctx.SeedInitialData();
-                }
-            } 
+            //    if (app.Environment.IsDevelopment())
+            //    {
+            //        ctx.SeedInitialData();
+            //    }
+            //} 
             #endregion
 
             // Configure the HTTP request pipeline.
@@ -72,6 +72,9 @@ namespace VisualAcademy
                 app.UseMigrationsEndPoint();
                 app.UseSwagger();
                 app.UseSwaggerUI();
+
+                // 시스템 기본 제공 사용자 및 역할 만들기(처음 애플리케이션 가동할 때)
+                await CreateBuiltInUsersAndRoles(app);
             }
             else
             {
@@ -96,5 +99,137 @@ namespace VisualAcademy
 
             app.Run();
         }
+
+        #region Create BuiltIn Users and Roles 
+        /// <summary>
+        /// 내장 사용자 및 그룹(역할) 생성
+        /// </summary>
+        private static async Task CreateBuiltInUsersAndRoles(WebApplication app)
+        {
+            using var serviceScope = app.Services.CreateScope();
+
+            var serviceProvider = serviceScope.ServiceProvider;
+
+            //[0] DbContext 개체 생성
+            var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.EnsureCreated(); // 데이터베이스가 생성되어 있는지 확인 
+
+            // 기본 내장 사용자 및 역할이 하나도 없으면(즉, 처음 데이터베이스 생성이라면)
+            if (!dbContext.Users.Any() && !dbContext.Roles.Any())
+            {
+                string domainName = "hawaso.com";
+
+                //[1] Groups(Roles)
+                var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+                //[1][1] ('Administrators', '관리자 그룹', 'Group', '응용 프로그램을 총 관리하는 관리 그룹 계정')
+                //[1][2] ('Everyone', '전체 사용자 그룹', 'Group', '응용 프로그램을 사용하는 모든 사용자 그룹 계정')
+                //[1][3] ('Users', '일반 사용자 그룹', 'Group', '일반 사용자 그룹 계정')
+                //[1][4] ('Guests', '게스트 그룹', 'Group', '게스트 사용자 그룹 계정')
+                //string[] roleNames = { Dul.Roles.Administrators.ToString(), Dul.Roles.Everyone.ToString(), Dul.Roles.Users.ToString(), Dul.Roles.Guests.ToString() };
+                //foreach (var roleName in roleNames)
+                //{
+                //    var roleExist = await roleManager.RoleExistsAsync(roleName);
+                //    if (!roleExist)
+                //    {
+                //        await roleManager.CreateAsync(new ApplicationRole { Name = roleName, NormalizedName = roleName.ToUpper(), Description = "" }); // 빌트인 그룹 생성
+                //    }
+                //}
+                //[1][1] Administrators
+                var administrators = new ApplicationRole
+                {
+                    Name = Dul.Roles.Administrators.ToString(),
+                    NormalizedName = Dul.Roles.Administrators.ToString().ToUpper(),
+                    Description = "응용 프로그램을 총 관리하는 관리 그룹 계정"
+                };
+                if (!(await roleManager.RoleExistsAsync(administrators.Name)))
+                {
+                    await roleManager.CreateAsync(administrators);
+                }
+                //[1][2] Everyone
+                var everyone = new ApplicationRole
+                {
+                    Name = Dul.Roles.Everyone.ToString(),
+                    NormalizedName = Dul.Roles.Everyone.ToString().ToUpper(),
+                    Description = "응용 프로그램을 사용하는 모든 사용자 그룹 계정"
+                };
+                if (!(await roleManager.RoleExistsAsync(everyone.Name)))
+                {
+                    await roleManager.CreateAsync(everyone);
+                }
+                //[1][3] Users
+                var users = new ApplicationRole
+                {
+                    Name = Dul.Roles.Users.ToString(),
+                    NormalizedName = Dul.Roles.Users.ToString().ToUpper(),
+                    Description = "일반 사용자 그룹 계정"
+                };
+                if (!(await roleManager.RoleExistsAsync(users.Name)))
+                {
+                    await roleManager.CreateAsync(users);
+                }
+                //[1][4] Guests
+                var guests = new ApplicationRole
+                {
+                    Name = Dul.Roles.Guests.ToString(),
+                    NormalizedName = Dul.Roles.Guests.ToString().ToUpper(),
+                    Description = "게스트 사용자 그룹 계정"
+                };
+                if (!(await roleManager.RoleExistsAsync(guests.Name)))
+                {
+                    await roleManager.CreateAsync(guests);
+                }
+
+                //[2] Users
+                var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                //[2][1] Administrator
+                // ('Administrator', '관리자', 'User', '응용 프로그램을 총 관리하는 사용자 계정')
+                ApplicationUser? administrator = await userManager.FindByEmailAsync($"administrator@{domainName}");
+                if (administrator == null)
+                {
+                    administrator = new ApplicationUser()
+                    {
+                        UserName = $"administrator@{domainName}",
+                        Email = $"administrator@{domainName}",
+                        EmailConfirmed = true, // 기본 템플릿에는 이메일 인증 과정을 거치기때문에 EmailConfirmed 속성을 true로 설정해야 함.
+                    };
+                    await userManager.CreateAsync(administrator, "Pa$$w0rd");
+                }
+
+                //[2][2] Guest
+                // ('Guest', '게스트 사용자', 'User', '게스트 사용자 계정')
+                ApplicationUser? guest = await userManager.FindByEmailAsync($"guest@{domainName}");
+                if (guest == null)
+                {
+                    guest = new ApplicationUser()
+                    {
+                        UserName = "Guest",
+                        Email = $"guest@{domainName}",
+                        EmailConfirmed = false,
+                    };
+                    await userManager.CreateAsync(guest, "Pa$$w0rd");
+                }
+
+                //[2][3] Anonymous
+                // ('Anonymous', '익명 사용자', 'User', '익명 사용자 계정')
+                ApplicationUser? anonymous = await userManager.FindByEmailAsync($"anonymous@{domainName}");
+                if (anonymous == null)
+                {
+                    anonymous = new ApplicationUser()
+                    {
+                        UserName = "Anonymous",
+                        Email = $"anonymous@{domainName}",
+                        EmailConfirmed = false,
+                    };
+                    await userManager.CreateAsync(anonymous, "Pa$$w0rd");
+                }
+
+                //[3] UsersInRoles: AspNetUserRoles Table
+                await userManager.AddToRoleAsync(administrator, Dul.Roles.Administrators.ToString());
+                await userManager.AddToRoleAsync(administrator, Dul.Roles.Users.ToString());
+                await userManager.AddToRoleAsync(guest, Dul.Roles.Guests.ToString());
+                await userManager.AddToRoleAsync(anonymous, Dul.Roles.Guests.ToString());
+            }
+        }
+        #endregion
     }
 }
