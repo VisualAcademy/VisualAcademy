247cd0ca0cfb0972f720a24e41c2ce21ae16a7f8
Park YongJune <redplus@redplus.net>
2023-01-24 01:27:45 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -11,68 +11,67 @@ using Microsoft.AspNetCore.WebUtilities;
 using System.Text;
 using VisualAcademy.Areas.Identity.Models;
 
-namespace VisualAcademy.Areas.Identity.Pages.Account
+namespace VisualAcademy.Areas.Identity.Pages.Account;
+
+[AllowAnonymous]
+public class RegisterConfirmationModel : PageModel
 {
-    [AllowAnonymous]
-    public class RegisterConfirmationModel : PageModel
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IEmailSender _sender;
+
+    public RegisterConfirmationModel(UserManager<ApplicationUser> userManager, IEmailSender sender)
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IEmailSender _sender;
+        _userManager = userManager;
+        _sender = sender;
+    }
 
-        public RegisterConfirmationModel(UserManager<ApplicationUser> userManager, IEmailSender sender)
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public bool DisplayConfirmAccountLink { get; set; }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public string EmailConfirmationUrl { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(string email, string returnUrl = null)
+    {
+        if (email == null)
         {
-            _userManager = userManager;
-            _sender = sender;
+            return RedirectToPage("/Index");
+        }
+        returnUrl = returnUrl ?? Url.Content("~/");
+
+        var user = await _userManager.FindByEmailAsync(email);
+        if (user == null)
+        {
+            return NotFound($"Unable to load user with email '{email}'.");
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string Email { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public bool DisplayConfirmAccountLink { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string EmailConfirmationUrl { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(string email, string returnUrl = null)
+        Email = email;
+        // Once you add a real email sender, you should remove this code that lets you confirm the account
+        DisplayConfirmAccountLink = true;
+        if (DisplayConfirmAccountLink)
         {
-            if (email == null)
-            {
-                return RedirectToPage("/Index");
-            }
-            returnUrl = returnUrl ?? Url.Content("~/");
-
-            var user = await _userManager.FindByEmailAsync(email);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with email '{email}'.");
-            }
-
-            Email = email;
-            // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
-            if (DisplayConfirmAccountLink)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                EmailConfirmationUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                    protocol: Request.Scheme);
-            }
-
-            return Page();
+            var userId = await _userManager.GetUserIdAsync(user);
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            EmailConfirmationUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                protocol: Request.Scheme);
         }
+
+        return Page();
     }
 }
