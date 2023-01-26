743a29d22ddf8a614ef529bb1e8db7a73f55fdf4
Park YongJune <redplus@redplus.net>
2023-01-27 07:59:18 +0900

    .

--- src/VisualAcademy/VisualAcademy/Areas/Identity/Services/EmailSender.cs
+++ src/VisualAcademy/VisualAcademy/Areas/Identity/Services/EmailSender.cs
@@ -12,6 +12,10 @@ namespace VisualAcademy.Areas.Identity.Services
             string subject,
             string htmlMessage)
         {
+            // 여기 코드는
+            // 직접 SMTP 서버를 구성하거나,
+            // SendGrid와 같은 클라우드 서비스를 사용하면 됩니다.
+            // 다만, 학습 차원에서는 여기에 중단점 설정 후 htmlMessage를 보고 Confirm 절차를 진행해도 됩니다.
             return Task.CompletedTask;
         }
     }
