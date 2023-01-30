50e5e851c41351667eb7f63c53171c9a501df653
Park YongJune <redplus@redplus.net>
2023-01-30 19:24:32 +0900

    #nullable disable

--- src/VisualAcademy/VisualAcademy/Models/ManageViewModels/EnableAuthenticatorViewModel.cs
+++ src/VisualAcademy/VisualAcademy/Models/ManageViewModels/EnableAuthenticatorViewModel.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿#nullable disable
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace VisualAcademy.Models.ManageViewModels
 {
