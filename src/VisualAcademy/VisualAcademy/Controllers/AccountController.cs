dfa5aee04411e3761a1807d15ef504759c936534
Park YongJune <redplus@redplus.net>
2023-01-30 19:24:06 +0900

    #nullable disable

--- src/VisualAcademy/VisualAcademy/Controllers/AccountController.cs
+++ src/VisualAcademy/VisualAcademy/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿#nullable disable
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
--- src/VisualAcademy/VisualAcademy/Extensions/UrlHelperExtensions.cs
+++ src/VisualAcademy/VisualAcademy/Extensions/UrlHelperExtensions.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+#nullable disable
 using VisualAcademy.Controllers;
 
 namespace Microsoft.AspNetCore.Mvc
