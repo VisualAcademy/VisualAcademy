d1968e274be4e60471a14a624a09abbff6e31ce8
Park YongJune <redplus@redplus.net>
2023-02-05 08:14:41 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Models/ManageViewModels/IndexViewModel.cs
+++ src/VisualAcademy/VisualAcademy/Models/ManageViewModels/IndexViewModel.cs
@@ -1,22 +1,21 @@
 ﻿#nullable disable
 using System.ComponentModel.DataAnnotations;
 
-namespace VisualAcademy.Models.ManageViewModels
+namespace VisualAcademy.Models.ManageViewModels;
+
+public class IndexViewModel
 {
-    public class IndexViewModel
-    {
-        public string Username { get; set; }
+    public string Username { get; set; }
 
-        public bool IsEmailConfirmed { get; set; }
+    public bool IsEmailConfirmed { get; set; }
 
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
 
-        [Phone]
-        [Display(Name = "Phone number")]
-        public string PhoneNumber { get; set; }
+    [Phone]
+    [Display(Name = "Phone number")]
+    public string PhoneNumber { get; set; }
 
-        public string StatusMessage { get; set; }
-    }
+    public string StatusMessage { get; set; }
 }
