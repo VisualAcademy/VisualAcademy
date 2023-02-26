27b05601be07a3260d215f0a2dfba6eadd6a2e60
Park YongJune <redplus@redplus.net>
2023-02-26 19:01:39 +0900

    .

--- src/VisualAcademy/ArticleApp.Models/ArticleAppDbContext.cs
+++ src/VisualAcademy/ArticleApp.Models/ArticleAppDbContext.cs
@@ -30,7 +30,7 @@ namespace ArticleApp.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // 닷넷 프레임워크 기반에서 호출되는 코드 영역: 
+            // 닷넷 프레임워크 기반에서 호출되는 코드 영역(.NET 6 이후로는 사용하지 않음): 
             // App.config 또는 Web.config의 연결 문자열 사용
             // 직접 데이터베이스 연결문자열 설정 가능
             if (!optionsBuilder.IsConfigured)
