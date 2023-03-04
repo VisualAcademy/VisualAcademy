b04cf16dd84518484cc633c235ef08caec721905
Park YongJune <redplus@redplus.net>
2023-03-04 11:32:21 +0900

    ButtonWithModal

---
+++ src/VisualAcademy/VisualAcademy/Controllers/BlazorDemoController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Controllers {
+    public class BlazorDemoController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
