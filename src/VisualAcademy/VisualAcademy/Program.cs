aa89977f26dddef037a5b8b95537bfd33aa0ce93
Park YongJune <redplus@redplus.net>
2023-03-08 11:27:26 +0900

    Code Cleanup

--- src/VisualAcademy/VisualAcademy/Program.cs
+++ src/VisualAcademy/VisualAcademy/Program.cs
@@ -13,11 +13,13 @@ using VisualAcademy.Models.Candidates;
 namespace VisualAcademy {
     public class Program {
         public static async Task Main(string[] args) {
+            // ASP.NET Core 앱의 구성, 서비스 구성, 미들웨어 구성 등을 담당한다.
             var builder = WebApplication.CreateBuilder(args);
 
             //[!] ConfigureServices... Startup.cs 파일에서 ConfigureServices 메서드 영역: 
 
             // Add services to the container.
+            // 컨테이너에 서비스를 추가한다.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
@@ -97,6 +99,7 @@ namespace VisualAcademy {
             builder.Services.AddTransient<PortfolioServiceJsonFile>(); // DI Container
             #endregion
 
+            // 앱 빌드
             var app = builder.Build();
 
             //[!] Configure... Startup.cs 파일에서 Configure 메서드 영역: 
@@ -121,7 +124,9 @@ namespace VisualAcademy {
             #endregion
 
             // Configure the HTTP request pipeline.
+            // HTTP 요청 파이프라인을 구성한다.
             if (app.Environment.IsDevelopment()) {
+                // 개발 환경에서는 마이그레이션 엔드포인트를 사용한다.
                 app.UseMigrationsEndPoint();
                 app.UseSwagger();
                 app.UseSwaggerUI();
@@ -130,11 +135,13 @@ namespace VisualAcademy {
                 await CreateBuiltInUsersAndRoles(app);
             }
             else {
+                // 운영 환경에서는 예외 처리 미들웨어와 HSTS 미들웨어를 사용한다.
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            // HTTPS 리디렉션과 정적 파일 미들웨어를 사용한다.
             app.UseHttpsRedirection();
 
             // 미들웨어 추가 
@@ -143,8 +150,10 @@ namespace VisualAcademy {
 
             app.UseRouting();
 
+            // 인증과 권한 미들웨어를 사용한다.
             app.UseAuthorization();
 
+            // 컨트롤러 및 Razor 페이지 미들웨어를 사용한다.
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}"); // MVC 라우팅
@@ -152,6 +161,7 @@ namespace VisualAcademy {
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
 
+            // 앱 실행
             app.Run();
         }
 
