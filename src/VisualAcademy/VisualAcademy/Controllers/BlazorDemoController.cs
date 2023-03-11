8aed045ffc64e0eba60526113c58899f587256cc
Park YongJune <redplus@redplus.net>
2023-03-12 02:51:57 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Controllers/BlazorDemoController.cs
+++ src/VisualAcademy/VisualAcademy/Controllers/BlazorDemoController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace VisualAcademy.Controllers {
-    public class BlazorDemoController : Controller {
-        public IActionResult Index() => View();
-    }
+namespace VisualAcademy.Controllers; 
+public class BlazorDemoController : Controller {
+    public IActionResult Index() => View();
 }
