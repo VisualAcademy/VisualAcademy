112a6fbb77cac43e0e35ad0ee6900324b2492620
Park YongJune <redplus@redplus.net>
2023-03-12 23:58:05 +0900

    Delete page

---
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Delete.cshtml.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VisualAcademy.Data;
+using VisualAcademy.Models;
+
+namespace VisualAcademy.Pages.Tenants {
+    public class DeleteModel : PageModel {
+        private readonly ApplicationDbContext _context;
+
+        public DeleteModel(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        [BindProperty]
+        public TenantModel TenantModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            TenantModel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (TenantModel == null) {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(long? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            TenantModel = await _context.Tenants.FindAsync(id);
+
+            if (TenantModel != null) {
+                _context.Tenants.Remove(TenantModel);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
