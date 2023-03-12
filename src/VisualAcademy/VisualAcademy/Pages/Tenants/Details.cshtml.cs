14e16ebe1cf0d8e4008471af7bf8548325b5e0d5
Park YongJune <redplus@redplus.net>
2023-03-12 23:51:05 +0900

    Details

---
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Details.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VisualAcademy.Data;
+using VisualAcademy.Models;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Pages.Tenants {
+    public class DetailsModel : PageModel {
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public TenantModel TenantModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            TenantModel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (TenantModel == null) {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
