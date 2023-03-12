d841fb223a9f160c9f144e98f6886dd426af836b
Park YongJune <redplus@redplus.net>
2023-03-12 23:46:07 +0900

    Create Page

--- src/VisualAcademy/VisualAcademy/Models/TenantModel.cs
+++ src/VisualAcademy/VisualAcademy/Models/TenantModel.cs
@@ -4,35 +4,35 @@ namespace VisualAcademy.Models {
     public class TenantModel {
         public long Id { get; set; }
 
-        [Required]
+        //[Required]
         [Display(Name = "Connection String")]
-        public string ConnectionString { get; set; }
+        public string? ConnectionString { get; set; }
 
         [Required]
         [Display(Name = "Name")]
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        [Required]
+        //[Required]
         [Display(Name = "Authentication Header")]
-        public string AuthenticationHeader { get; set; }
+        public string? AuthenticationHeader { get; set; }
 
-        [Required]
+        //[Required]
         [Display(Name = "Account ID")]
-        public string AccountID { get; set; }
+        public string? AccountID { get; set; }
 
-        [Required]
+        //[Required]
         [Display(Name = "Global Search Connection String")]
-        public string GSConnectionString { get; set; }
+        public string? GSConnectionString { get; set; }
 
-        [Required]
+        //[Required]
         [Display(Name = "Report Writer URL")]
-        public string ReportWriterURL { get; set; }
+        public string? ReportWriterURL { get; set; }
 
-        [Required]
+        //[Required]
         [Display(Name = "Badge Photo Type")]
-        public string BadgePhotoType { get; set; }
+        public string? BadgePhotoType { get; set; }
 
-        [Display(Name = "Portal Name")]
-        public string PortalName { get; set; }
+        //[Display(Name = "Portal Name")]
+        public string? PortalName { get; set; }
     }
 }
---
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Create.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VisualAcademy.Data;
+using VisualAcademy.Models;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Pages.Tenants {
+    public class CreateModel : PageModel {
+        private readonly ApplicationDbContext _context;
+
+        public CreateModel(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IActionResult OnGet() {
+            return Page();
+        }
+
+        [BindProperty]
+        public TenantModel TenantModel { get; set; }
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
+                return Page();
+            }
+
+            _context.Tenants.Add(TenantModel);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
