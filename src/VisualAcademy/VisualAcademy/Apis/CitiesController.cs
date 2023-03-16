2bcd2ba4467aa00f034f6590968809017758d3c9
Park YongJune <redplus@redplus.net>
2023-03-16 12:10:22 +0900

    파일 범위 네임스페이스로 변환

--- src/VisualAcademy/VisualAcademy/Apis/CitiesController.cs
+++ src/VisualAcademy/VisualAcademy/Apis/CitiesController.cs
@@ -2,90 +2,89 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace VisualAcademy.Apis
+namespace VisualAcademy.Apis;
+
+public class City
 {
-    public class City
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+[Authorize(Roles = "Administrators")]
+[ApiController]
+[Route("api/[controller]")]
+public class CitiesController : ControllerBase
+{
+    private readonly List<City> _cities;
+
+    public CitiesController()
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        _cities = new List<City>
+        {
+            new City { Id = 1, Name = "Seoul" },
+            new City { Id = 2, Name = "New York" },
+            new City { Id = 3, Name = "London" },
+            new City { Id = 4, Name = "Paris" },
+            new City { Id = 5, Name = "Tokyo" },
+        };
     }
 
-    [Authorize(Roles = "Administrators")]
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CitiesController : ControllerBase
+    // GET api/cities
+    [HttpGet]
+    public ActionResult<IEnumerable<City>> GetCities() => _cities;
+
+    // GET api/cities/5
+    [HttpGet("{id}")]
+    public ActionResult<City> GetCity(int id)
     {
-        private readonly List<City> _cities;
+        var city = _cities.FirstOrDefault(c => c.Id == id);
 
-        public CitiesController()
+        if (city == null)
         {
-            _cities = new List<City>
-            {
-                new City { Id = 1, Name = "Seoul" },
-                new City { Id = 2, Name = "New York" },
-                new City { Id = 3, Name = "London" },
-                new City { Id = 4, Name = "Paris" },
-                new City { Id = 5, Name = "Tokyo" },
-            };
+            return NotFound();
         }
 
-        // GET api/cities
-        [HttpGet]
-        public ActionResult<IEnumerable<City>> GetCities() => _cities;
+        return city;
+    }
 
-        // GET api/cities/5
-        [HttpGet("{id}")]
-        public ActionResult<City> GetCity(int id)
+    // POST api/cities
+    [HttpPost]
+    public ActionResult<City> CreateCity([FromBody] City city)
+    {
+        city.Id = _cities.Max(c => c.Id) + 1;
+        _cities.Add(city);
+        return CreatedAtAction(nameof(GetCity), new { id = city.Id }, city);
+    }
+
+    // PUT api/cities/5
+    [HttpPut("{id}")]
+    public ActionResult UpdateCity(int id, [FromBody] City city)
+    {
+        var existingCity = _cities.FirstOrDefault(c => c.Id == id);
+
+        if (existingCity == null)
         {
-            var city = _cities.FirstOrDefault(c => c.Id == id);
-
-            if (city == null)
-            {
-                return NotFound();
-            }
-
-            return city;
+            return NotFound();
         }
 
-        // POST api/cities
-        [HttpPost]
-        public ActionResult<City> CreateCity([FromBody] City city)
+        existingCity.Name = city.Name;
+
+        return NoContent();
+    }
+
+    // DELETE api/cities/5
+    [HttpDelete("{id}")]
+    public ActionResult DeleteCity(int id)
+    {
+        var city = _cities.FirstOrDefault(c => c.Id == id);
+
+        if (city == null)
         {
-            city.Id = _cities.Max(c => c.Id) + 1;
-            _cities.Add(city);
-            return CreatedAtAction(nameof(GetCity), new { id = city.Id }, city);
+            return NotFound();
         }
 
-        // PUT api/cities/5
-        [HttpPut("{id}")]
-        public ActionResult UpdateCity(int id, [FromBody] City city)
-        {
-            var existingCity = _cities.FirstOrDefault(c => c.Id == id);
+        _cities.Remove(city);
 
-            if (existingCity == null)
-            {
-                return NotFound();
-            }
-
-            existingCity.Name = city.Name;
-
-            return NoContent();
-        }
-
-        // DELETE api/cities/5
-        [HttpDelete("{id}")]
-        public ActionResult DeleteCity(int id)
-        {
-            var city = _cities.FirstOrDefault(c => c.Id == id);
-
-            if (city == null)
-            {
-                return NotFound();
-            }
-
-            _cities.Remove(city);
-
-            return NoContent();
-        }
+        return NoContent();
     }
 }
