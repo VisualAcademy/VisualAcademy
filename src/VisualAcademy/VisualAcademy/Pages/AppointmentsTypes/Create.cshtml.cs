67e86cd04644737f34b1f00d14394777975fca59
Park YongJune <redplus@redplus.net>
2023-03-24 14:15:07 +0900

    Code Cleanup

--- src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Create.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Create.cshtml.cs
@@ -1,29 +1,34 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models.Tenants;
 using VisualAcademy.Repositories.Tenants;
 
-namespace VisualAcademy.Pages.AppointmentsTypes {
-    public class CreateModel : PageModel {
+namespace VisualAcademy.Pages.AppointmentsTypes
+{
+    public class CreateModel : PageModel
+    {
         private readonly IAppointmentTypeRepository _appointmentTypeRepository;
 
-        public CreateModel(IAppointmentTypeRepository appointmentTypeRepository) {
+        public CreateModel(IAppointmentTypeRepository appointmentTypeRepository)
+        {
             _appointmentTypeRepository = appointmentTypeRepository;
         }
 
-        public IActionResult OnGet() {
+        public IActionResult OnGet()
+        {
             return Page();
         }
 
         [BindProperty]
         public AppointmentTypeModel AppointmentType { get; set; }
 
-        public async Task<IActionResult> OnPostAsync() {
+        public async Task<IActionResult> OnPostAsync()
+        {
             long tenantId = 1; // tenantId 값을 1로 초기화합니다.
             AppointmentType.TenantId = tenantId;
 
-            if (!ModelState.IsValid) {
+            if (!ModelState.IsValid)
+            {
                 return Page();
             }
 
