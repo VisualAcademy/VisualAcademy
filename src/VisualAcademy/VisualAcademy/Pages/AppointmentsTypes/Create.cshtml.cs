75758313ab2f6ed55dbf6479d6f1853e13a8c868
Park YongJune <redplus@redplus.net>
2023-03-24 14:15:35 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Create.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Create.cshtml.cs
@@ -3,38 +3,37 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models.Tenants;
 using VisualAcademy.Repositories.Tenants;
 
-namespace VisualAcademy.Pages.AppointmentsTypes
+namespace VisualAcademy.Pages.AppointmentsTypes;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly IAppointmentTypeRepository _appointmentTypeRepository;
+
+    public CreateModel(IAppointmentTypeRepository appointmentTypeRepository)
     {
-        private readonly IAppointmentTypeRepository _appointmentTypeRepository;
+        _appointmentTypeRepository = appointmentTypeRepository;
+    }
 
-        public CreateModel(IAppointmentTypeRepository appointmentTypeRepository)
-        {
-            _appointmentTypeRepository = appointmentTypeRepository;
-        }
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
 
-        public IActionResult OnGet()
+    [BindProperty]
+    public AppointmentTypeModel AppointmentType { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        long tenantId = 1; // tenantId 값을 1로 초기화합니다.
+        AppointmentType.TenantId = tenantId;
+
+        if (!ModelState.IsValid)
         {
             return Page();
         }
 
-        [BindProperty]
-        public AppointmentTypeModel AppointmentType { get; set; }
+        await _appointmentTypeRepository.AddAsync(AppointmentType);
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            long tenantId = 1; // tenantId 값을 1로 초기화합니다.
-            AppointmentType.TenantId = tenantId;
-
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            await _appointmentTypeRepository.AddAsync(AppointmentType);
-
-            return RedirectToPage("./Index");
-        }
+        return RedirectToPage("./Index");
     }
 }
