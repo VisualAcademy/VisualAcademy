8315e5c79424720d752daf791d199cb89f2532e3
Park YongJune <redplus@redplus.net>
2023-03-31 06:33:39 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Index.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Index.cshtml.cs
@@ -2,19 +2,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models.Tenants;
 using VisualAcademy.Repositories.Tenants;
 
-namespace VisualAcademy.Pages.AppointmentsTypes {
-    public class IndexModel : PageModel {
-        private readonly IAppointmentTypeRepository _appointmentTypeRepository;
+namespace VisualAcademy.Pages.AppointmentsTypes; 
+public class IndexModel : PageModel {
+    private readonly IAppointmentTypeRepository _appointmentTypeRepository;
 
-        public IndexModel(IAppointmentTypeRepository appointmentTypeRepository) {
-            _appointmentTypeRepository = appointmentTypeRepository;
-        }
+    public IndexModel(IAppointmentTypeRepository appointmentTypeRepository) {
+        _appointmentTypeRepository = appointmentTypeRepository;
+    }
 
-        public IList<AppointmentTypeModel> AppointmentTypes { get; set; }
+    public IList<AppointmentTypeModel> AppointmentTypes { get; set; }
 
-        public async Task OnGetAsync() {
-            long tenantId = 1; // tenantId 값을 1로 초기화합니다.
-            AppointmentTypes = await _appointmentTypeRepository.GetAllAsync(tenantId);
-        }
+    public async Task OnGetAsync() {
+        long tenantId = 1; // tenantId 값을 1로 초기화합니다.
+        AppointmentTypes = await _appointmentTypeRepository.GetAllAsync(tenantId);
     }
 }
