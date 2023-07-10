ce1a15088e04ecb22b24a922d726041857927747
Park YongJune <redplus@redplus.net>
2023-07-10 17:12:14 +0900

    Code Cleanup

--- src/VisualAcademy/Persistence/DatabaseService.cs
+++ src/VisualAcademy/Persistence/DatabaseService.cs
@@ -3,21 +3,33 @@ using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Features.Buffets;
 
-namespace Persistence;
-
-public class DatabaseService : DbContext, IDatabaseService
+namespace Persistence
 {
-    public DatabaseService(DbContextOptions<DatabaseService> options) : base(options)
-        => Database.EnsureCreated(); // 인-메모리 데이터베이스 생성
-
-    public DbSet<Todo> Todos { get; set; }
-
-    public void Save() => this.SaveChanges();
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // DbContext와 IDatabaseService를 상속받는 DatabaseService 클래스를 선언합니다.
+    public class DatabaseService : DbContext, IDatabaseService
     {
-        base.OnModelCreating(modelBuilder);
+        // 생성자에서 DbContextOptions를 사용하여 기본 클래스의 생성자를 호출하고, 데이터베이스가 생성되도록 합니다.
+        public DatabaseService(DbContextOptions<DatabaseService> options) : base(options)
+        {
+            Database.EnsureCreated(); // 인-메모리 데이터베이스 생성
+        }
 
-        new TodoConfiguration().Configure(modelBuilder.Entity<Todo>());
+        // Todo 엔터티에 대한 DbSet를 선언합니다.
+        public DbSet<Todo> Todos { get; set; }
+
+        // 변경사항을 저장하는 Save 메서드를 선언합니다.
+        public void Save()
+        {
+            this.SaveChanges();
+        }
+
+        // 모델 생성 시 추가적인 구성을 위한 OnModelCreating 메서드를 오버라이드 합니다.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Todo 엔터티에 대한 추가적인 구성을 수행합니다.
+            new TodoConfiguration().Configure(modelBuilder.Entity<Todo>());
+        }
     }
 }
--- src/VisualAcademy/Persistence/Todos/TodoConfiguration.cs
+++ src/VisualAcademy/Persistence/Todos/TodoConfiguration.cs
@@ -2,18 +2,39 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Persistence.Features.Buffets;
-
-public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+namespace Persistence.Features.Buffets
 {
-    public void Configure(EntityTypeBuilder<Todo> builder)
+    // Todo 엔터티에 대한 EF Core 구성을 처리하는 TodoConfiguration 클래스를 선언합니다.
+    public class TodoConfiguration : IEntityTypeConfiguration<Todo>
     {
-        builder.HasKey(x => x.Id);
+        // Todo 엔터티를 구성하는 Configure 메서드를 선언합니다.
+        public void Configure(EntityTypeBuilder<Todo> builder)
+        {
+            // Id를 기본 키로 설정합니다.
+            builder.HasKey(x => x.Id);
 
-        builder.Property(b => b.Name).IsRequired().HasMaxLength(25);
+            // Name 속성은 필수이며, 최대 길이는 25입니다.
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(25);
 
-        builder.HasData(
-            new Todo { Id = 1, Name = "콩국물", IsComplete = true },
-            new Todo { Id = 2, Name = "멸치국물", IsComplete = false });
+            // 초기 데이터를 설정합니다.
+            // 데이터베이스가 처음 생성되는 시점에 이 데이터가 삽입됩니다.
+            builder.HasData
+            (
+                new Todo
+                {
+                    Id = 1,
+                    Name = "콩국물",
+                    IsComplete = true
+                },
+                new Todo
+                {
+                    Id = 2,
+                    Name = "멸치국물",
+                    IsComplete = false
+                }
+            );
+        }
     }
 }
