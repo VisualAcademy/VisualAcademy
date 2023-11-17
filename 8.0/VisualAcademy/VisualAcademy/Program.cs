c3e7b2e91ab00527b50d7c9285a68be6919789df
Park YongJune <redplus@redplus.net>
2023-11-17 22:51:13 +0900

    HTML content rendering

--- 8.0/VisualAcademy/VisualAcademy/Program.cs
+++ 8.0/VisualAcademy/VisualAcademy/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Content("<h1>Hello World!</h1>", "text/html"));
 
 app.Run();
