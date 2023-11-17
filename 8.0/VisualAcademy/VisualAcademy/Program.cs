5fd2830051178caf95f796497e2c16bbddb2a3d1
Park YongJune <redplus@redplus.net>
2023-11-18 06:07:02 +0900

    AddMvc

--- 8.0/VisualAcademy/VisualAcademy/Program.cs
+++ 8.0/VisualAcademy/VisualAcademy/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
+#region HTTP
 // C# 11.0 버전의 원시 문자열 리터럴(Raw string literals)
 string htmlTag = """
 <html lang="ko">
@@ -11,7 +17,23 @@ string htmlTag = """
 """;
 
 app.MapGet("/html-content-rendering", () => Results.Content(htmlTag, "text/html"));
+#endregion
 
-app.UseStaticFiles();
+#region HTML
+app.UseDefaultFiles();
+app.UseStaticFiles(); 
+#endregion
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
+
+#region MVC
+// /Mvc/Hello
+public class MvcController : Controller
+{
+    public string Hello() => "Hello, MVC";
+}
+#endregion
