2683ac6b841cffc2b7a214d268abc317fdebfcd4
Park YongJune <redplus@redplus.net>
2023-11-18 07:15:14 +0900

    Index view page

--- 8.0/VisualAcademy/VisualAcademy/Controllers/HomeController.cs
+++ 8.0/VisualAcademy/VisualAcademy/Controllers/HomeController.cs
@@ -4,6 +4,11 @@ namespace VisualAcademy.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
         public IActionResult Hello()
         {
             return Content("<h1>안녕하세요. MVC!</h1>", "text/html; charset=utf-8");
