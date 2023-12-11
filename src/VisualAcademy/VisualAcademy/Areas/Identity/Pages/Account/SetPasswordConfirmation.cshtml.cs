34d2ed078f301767c5657ffafe36a0b3fc74071f
Park YongJune <redplus@redplus.net>
2023-12-11 17:44:12 +0900

    .

--- src/VisualAcademy/VisualAcademy/Areas/Identity/Pages/Account/SetPasswordConfirmation.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Areas/Identity/Pages/Account/SetPasswordConfirmation.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace VisualAcademy.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class SetPasswordConfirmationModel : PageModel
     {
         public void OnGet()
