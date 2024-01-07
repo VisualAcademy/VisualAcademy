0da309f1e8666d52831af6107590af22f3e5a5a5
Park YongJune <redplus@redplus.net>
2024-01-08 06:45:27 +0900

    Code Cleanup

--- 8.0/IPRanges/blazor/VisualAcademy/VisualAcademy/Data/ApplicationUser.cs
+++ 8.0/IPRanges/blazor/VisualAcademy/VisualAcademy/Data/ApplicationUser.cs
@@ -1,27 +1,37 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace VisualAcademy.Data
+namespace VisualAcademy.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-        public string? FirstName { get; set; }
+    /// <summary>
+    /// 사용자의 이름
+    /// </summary>
+    public string? FirstName { get; set; }
 
-        public string? LastName { get; set; }
+    /// <summary>
+    /// 사용자의 성
+    /// </summary>
+    public string? LastName { get; set; }
 
-        public string? Timezone { get; set; }
+    /// <summary>
+    /// 사용자의 시간대
+    /// </summary>
+    public string? Timezone { get; set; }
 
-        // TODO: 
-        /// <summary>
-        /// 주소
-        /// </summary>
-        public string? Address { get; set; }
+    /// <summary>
+    /// 주소
+    /// </summary>
+    public string? Address { get; set; }
 
-        /// <summary>
-        /// 성별
-        /// </summary>
-        public string? Gender { get; set; }
+    /// <summary>
+    /// 성별
+    /// </summary>
+    public string? Gender { get; set; }
 
-        public long TenantId { get; set; }
-    }
+    /// <summary>
+    /// 테넌트 ID
+    /// </summary>
+    public long TenantId { get; set; }
 }
