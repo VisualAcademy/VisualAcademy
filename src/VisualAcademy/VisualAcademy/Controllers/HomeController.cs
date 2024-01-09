4376393da5b2a0e64eeb32ab6e4ffe3f1a12c4bf
Park YongJune <redplus@redplus.net>
2024-01-09 15:52:00 +0900

    기본 생성자 사용(및 필드 제거)

--- src/VisualAcademy/VisualAcademy/Controllers/HomeController.cs
+++ src/VisualAcademy/VisualAcademy/Controllers/HomeController.cs
@@ -4,12 +4,8 @@ using VisualAcademy.Models;
 
 namespace VisualAcademy.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger) => _logger = logger;
-
     public IActionResult Index() => View();
 
     public IActionResult Privacy() => View();
