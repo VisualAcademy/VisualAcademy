5439a2281e240da81dde94067b96c91777e7413b
Park YongJune <redplus@redplus.net>
2024-01-09 15:52:35 +0900

    기본 생성자 사용(및 필드 제거)

--- src/VisualAcademy/VisualAcademy/Controllers/WeatherForecastController.cs
+++ src/VisualAcademy/VisualAcademy/Controllers/WeatherForecastController.cs
@@ -5,16 +5,12 @@ namespace VisualAcademy.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger) => _logger = logger;
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
