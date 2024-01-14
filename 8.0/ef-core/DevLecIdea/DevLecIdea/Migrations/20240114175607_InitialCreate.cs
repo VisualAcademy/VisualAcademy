b567289061e26a55ed30eff7ee387ef35a72cb70
Park YongJune <redplus@redplus.net>
2024-01-15 03:04:02 +0900

    Idea 모델 클래스에서 Ideas 테이블까지

---
+++ 8.0/ef-core/DevLecIdea/DevLecIdea/Migrations/20240114175607_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevLecIdea.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ideas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ideas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ideas");
+        }
+    }
+}
---
+++ 8.0/ef-core/DevLecIdea/DevLecIdea/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class Idea
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!; 
+}
+
+public class IdeaDbContext : DbContext
+{
+    public DbSet<Idea> Ideas { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = @"Server=(localdb)\mssqllocaldb;
+            Database=DevLecIdea;Trusted_Connection=True";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        using (var db = new IdeaDbContext())
+        {
+            var idea = new Idea { Title = "Entity Framework Core 학습!" };
+            db.Ideas.Add(idea);
+            db.SaveChanges();
+
+            foreach (var item in db.Ideas)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+}
