a9077ade9855dfa0f1071ea0008cee22006f5971
Park YongJune <redplus@redplus.net>
2024-01-26 15:49:46 +0900

    Use expression body for method

--- src/VisualAcademy/VisualAcademy/Controllers/HomeController.cs
+++ src/VisualAcademy/VisualAcademy/Controllers/HomeController.cs
@@ -7,8 +7,5 @@ public class HomeController(ILogger<HomeController> logger) : Controller
     public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
