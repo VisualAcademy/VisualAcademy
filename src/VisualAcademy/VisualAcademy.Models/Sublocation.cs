1150b72f0af3008019e4396243e54d6614f0e540
Park YongJune <redplus@redplus.net>
2024-02-02 15:49:35 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy.Models/Sublocation.cs
+++ src/VisualAcademy/VisualAcademy.Models/Sublocation.cs
@@ -1,27 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace VisualAcademy.Models
+namespace VisualAcademy.Models;
+
+[Table("Sublocation")]
+public partial class Sublocation
 {
-    [Table("Sublocation")]
-    public partial class Sublocation
-    {
-        [Key]
-        public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-        [Display(Name = "Sublocation Name")]
-        [Column("Sublocation")]
-        public string? SublocationName { get; set; }
+    [Display(Name = "Sublocation Name")]
+    [Column("Sublocation")]
+    public string? SublocationName { get; set; }
 
-        public bool Active { get; set; } = false;
+    public bool Active { get; set; } = false;
 
-        public string? Location { get; set; }
+    public string? Location { get; set; }
 
-        public string? Property { get; set; }
+    public string? Property { get; set; }
 
-        [Display(Name = "Location")]
-        public Location? LocationRef { get; set; }
-        [Display(Name = "Location")]
-        public int? LocationId { get; set; } = 0;
-    }
+    [Display(Name = "Location")]
+    public Location? LocationRef { get; set; }
+    [Display(Name = "Location")]
+    public int? LocationId { get; set; } = 0;
 }
