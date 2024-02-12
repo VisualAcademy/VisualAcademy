947e2ecdf7a1b4b22d106338e4bb89b945ee16d6
Park YongJune <redplus@redplus.net>
2024-02-13 04:17:28 +0900

    Code Cleanup

--- src/VisualAcademy/VisualAcademy/Data/ApplicationUser.cs
+++ src/VisualAcademy/VisualAcademy/Data/ApplicationUser.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace VisualAcademy.Data;
+﻿namespace VisualAcademy.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
