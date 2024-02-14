22a87bb83438e911180047e548a4e9f314284c72
Park YongJune <redplus@redplus.net>
2024-02-14 23:07:15 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Models/Tenants/AppointmentTypeModel.cs
+++ src/VisualAcademy/VisualAcademy/Models/Tenants/AppointmentTypeModel.cs
@@ -1,25 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace VisualAcademy.Models.Tenants {
-    public class AppointmentTypeModel {
-        [Key]
-        public long Id { get; set; }
+namespace VisualAcademy.Models.Tenants;
 
-        [Required]
-        [Display(Name = "Appointment Type Name")]
-        [StringLength(50)]
-        public string AppointmentTypeName { get; set; }
+public class AppointmentTypeModel
+{
+    [Key]
+    public long Id { get; set; }
 
-        [Required]
-        public bool IsActive { get; set; } = true;
+    [Required]
+    [Display(Name = "Appointment Type Name")]
+    [StringLength(50)]
+    public string AppointmentTypeName { get; set; }
 
-        //[Required]
-        [Display(Name = "Date Created")]
-        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    [Required]
+    public bool IsActive { get; set; } = true;
 
-        [Display(Name = "Tenant")]
-        public long? TenantId { get; set; } = null!;
+    //[Required]
+    [Display(Name = "Date Created")]
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
 
-        public TenantModel? Tenant { get; set; } = null!;
-    }
+    [Display(Name = "Tenant")]
+    public long? TenantId { get; set; } = null!;
+
+    public TenantModel? Tenant { get; set; } = null!;
 }
