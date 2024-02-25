7a816eac1e64ec685319852587c1c6f9290710e6
Park YongJune <redplus@redplus.net>
2024-02-25 20:08:51 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy.Models.Notes/02_02_INoteCommentRepository.cs
+++ src/VisualAcademy/VisualAcademy.Models.Notes/02_02_INoteCommentRepository.cs
@@ -1,12 +1,11 @@
-﻿namespace Hawaso.Models.Notes
+﻿namespace Hawaso.Models.Notes;
+
+public interface INoteCommentRepository
 {
-    public interface INoteCommentRepository
-    {
-        void AddNoteComment(NoteComment model);
-        int DeleteNoteComment(int boardId, int id, string password);
-        int GetCountBy(int boardId, int id, string password);
-        List<NoteComment> GetNoteComments(int boardId);
-        List<NoteComment>? GetRecentComments();
-        List<NoteComment> GetRecentCommentsNoCache();
-    }
+    void AddNoteComment(NoteComment model);
+    int DeleteNoteComment(int boardId, int id, string password);
+    int GetCountBy(int boardId, int id, string password);
+    List<NoteComment> GetNoteComments(int boardId);
+    List<NoteComment>? GetRecentComments();
+    List<NoteComment> GetRecentCommentsNoCache();
 }
