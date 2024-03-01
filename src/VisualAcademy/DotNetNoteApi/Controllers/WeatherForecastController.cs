d0edddac6834b5e9480a088727251f7c7b398b7b
Park YongJune <redplus@redplus.net>
2024-03-02 07:27:51 +0900

    Use expression body for method

--- src/VisualAcademy/DotNetNoteApi/Controllers/WeatherForecastController.cs
+++ src/VisualAcademy/DotNetNoteApi/Controllers/WeatherForecastController.cs
@@ -15,14 +15,10 @@ public class WeatherForecastController(ILogger<WeatherForecastController> logger
     };
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public IEnumerable<WeatherForecast> Get() => Enumerable.Range(1, 5).Select(index => new WeatherForecast
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    }).ToArray();
 }
