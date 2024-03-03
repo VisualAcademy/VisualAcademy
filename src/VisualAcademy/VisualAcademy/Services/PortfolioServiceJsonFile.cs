8f70441316884a4f49d19b212f8726113c01f55d
Park YongJune <redplus@redplus.net>
2024-03-04 06:33:27 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Services/PortfolioServiceJsonFile.cs
+++ src/VisualAcademy/VisualAcademy/Services/PortfolioServiceJsonFile.cs
@@ -1,59 +1,58 @@
 ﻿using RedPlus.Models;
 using System.Text.Json;
 
-namespace RedPlus.Services
+namespace RedPlus.Services;
+
+public class PortfolioServiceJsonFile
 {
-    public class PortfolioServiceJsonFile
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public PortfolioServiceJsonFile(IWebHostEnvironment webHostEnvironment)
     {
-        private readonly IWebHostEnvironment _webHostEnvironment;
+        this._webHostEnvironment = webHostEnvironment;
+    }
 
-        public PortfolioServiceJsonFile(IWebHostEnvironment webHostEnvironment)
+    /// <summary>
+    /// wwwroot/Portfolios/portfolios.json 파일의 물리적인 경로 읽어오기 
+    /// </summary>
+    private string JsonFileName
+    {
+        get
         {
-            this._webHostEnvironment = webHostEnvironment;
+            return Path.Combine(_webHostEnvironment.WebRootPath, "Portfolios", "portfolios.json");
+        }
+    }
+
+    public IEnumerable<Portfolio> GetPortfolios()
+    {
+        // Using 선언: https://docs.microsoft.com/ko-kr/dotnet/csharp/whats-new/csharp-8#using-declarations
+        using var jsonFileReader = File.OpenText(JsonFileName);
+        var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+        var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
+        return portfolios;
+    }
+
+    public void AddRating(int portfolioId, int rating)
+    {
+        var portfolios = GetPortfolios();
+
+        if (portfolios.First(p => p.Id == portfolioId).Ratings == null)
+        {
+            portfolios.First(p => p.Id == portfolioId).Ratings = new int[] { rating };
+        }
+        else
+        {
+            var ratings = portfolios.First(p => p.Id == portfolioId).Ratings.ToList();
+            ratings.Add(rating);
+            portfolios.First(p => p.Id == portfolioId).Ratings = ratings.ToArray();
         }
 
-        /// <summary>
-        /// wwwroot/Portfolios/portfolios.json 파일의 물리적인 경로 읽어오기 
-        /// </summary>
-        private string JsonFileName
-        {
-            get
+        using var outputStream = File.OpenWrite(JsonFileName);
+        JsonSerializer.Serialize<IEnumerable<Portfolio>>(
+            new Utf8JsonWriter(outputStream, new JsonWriterOptions
             {
-                return Path.Combine(_webHostEnvironment.WebRootPath, "Portfolios", "portfolios.json");
-            }
-        }
-
-        public IEnumerable<Portfolio> GetPortfolios()
-        {
-            // Using 선언: https://docs.microsoft.com/ko-kr/dotnet/csharp/whats-new/csharp-8#using-declarations
-            using var jsonFileReader = File.OpenText(JsonFileName);
-            var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-            var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
-            return portfolios;
-        }
-
-        public void AddRating(int portfolioId, int rating)
-        {
-            var portfolios = GetPortfolios();
-
-            if (portfolios.First(p => p.Id == portfolioId).Ratings == null)
-            {
-                portfolios.First(p => p.Id == portfolioId).Ratings = new int[] { rating };
-            }
-            else
-            {
-                var ratings = portfolios.First(p => p.Id == portfolioId).Ratings.ToList();
-                ratings.Add(rating);
-                portfolios.First(p => p.Id == portfolioId).Ratings = ratings.ToArray();
-            }
-
-            using var outputStream = File.OpenWrite(JsonFileName);
-            JsonSerializer.Serialize<IEnumerable<Portfolio>>(
-                new Utf8JsonWriter(outputStream, new JsonWriterOptions
-                {
-                    SkipValidation = true,
-                    Indented = true
-                }), portfolios);
-        }
+                SkipValidation = true,
+                Indented = true
+            }), portfolios);
     }
 }
