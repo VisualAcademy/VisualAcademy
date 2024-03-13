ee33316dee4c93d17709787ee209433e7fddcff3
Park YongJune <redplus@redplus.net>
2024-03-13 17:03:59 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Delete.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Delete.cshtml.cs
@@ -2,38 +2,37 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models.Tenants;
 using VisualAcademy.Repositories.Tenants;
 
-namespace VisualAcademy.Pages.AppointmentsTypes
+namespace VisualAcademy.Pages.AppointmentsTypes;
+
+public class DeleteModel : PageModel
 {
-    public class DeleteModel : PageModel
+    private readonly IAppointmentTypeRepository _appointmentTypeRepository;
+
+    public DeleteModel(IAppointmentTypeRepository appointmentTypeRepository) => _appointmentTypeRepository = appointmentTypeRepository;
+
+    [BindProperty]
+    public AppointmentTypeModel AppointmentType { get; set; }
+
+    public long TenantId { get; set; } = 1;
+
+    public async Task<IActionResult> OnGetAsync(long id)
     {
-        private readonly IAppointmentTypeRepository _appointmentTypeRepository;
+        TenantId = 1;
+        AppointmentType = await _appointmentTypeRepository.GetByIdAsync(id, TenantId);
 
-        public DeleteModel(IAppointmentTypeRepository appointmentTypeRepository) => _appointmentTypeRepository = appointmentTypeRepository;
-
-        [BindProperty]
-        public AppointmentTypeModel AppointmentType { get; set; }
-
-        public long TenantId { get; set; } = 1;
-
-        public async Task<IActionResult> OnGetAsync(long id)
+        if (AppointmentType == null)
         {
-            TenantId = 1;
-            AppointmentType = await _appointmentTypeRepository.GetByIdAsync(id, TenantId);
-
-            if (AppointmentType == null)
-            {
-                return NotFound();
-            }
-
-            return Page();
+            return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(long id)
-        {
-            TenantId = 1;
-            await _appointmentTypeRepository.DeleteAsync(id, TenantId);
+        return Page();
+    }
 
-            return RedirectToPage("./Index");
-        }
+    public async Task<IActionResult> OnPostAsync(long id)
+    {
+        TenantId = 1;
+        await _appointmentTypeRepository.DeleteAsync(id, TenantId);
+
+        return RedirectToPage("./Index");
     }
 }
