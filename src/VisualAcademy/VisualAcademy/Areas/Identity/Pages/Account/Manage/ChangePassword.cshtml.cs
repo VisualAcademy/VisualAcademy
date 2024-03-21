679197e14bc8a79c7044bd6f53dbd0f7ebc62624
Park YongJune <redplus@redplus.net>
2024-03-22 03:35:02 +0900

    Remove unnecessary usings

--- src/VisualAcademy/VisualAcademy/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -2,11 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-using VisualAcademy.Data;
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage;
 
