9138fe5e06af659690873bd2ce5b6963721b6780
Park YongJune <redplus@redplus.net>
2024-03-24 06:06:38 +0900

    리무브 언니세서리 유징스

--- src/VisualAcademy/VisualAcademy/Components/Account/IdentityRevalidatingAuthenticationStateProvider.cs
+++ src/VisualAcademy/VisualAcademy/Components/Account/IdentityRevalidatingAuthenticationStateProvider.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
-using System.Security.Claims;
-using VisualAcademy.Data;
 
 namespace VisualAcademy.Components.Account
 {
