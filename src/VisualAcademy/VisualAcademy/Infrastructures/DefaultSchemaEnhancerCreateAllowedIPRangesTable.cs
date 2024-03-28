e5659a249f3ffe8026f1a7887fdce06c68d78f42
Park YongJune <redplus@redplus.net>
2024-03-28 18:07:12 +0900

    Remove unnecessary usings

--- src/VisualAcademy/VisualAcademy/Infrastructures/DefaultSchemaEnhancerCreateAllowedIPRangesTable.cs
+++ src/VisualAcademy/VisualAcademy/Infrastructures/DefaultSchemaEnhancerCreateAllowedIPRangesTable.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Data.SqlClient;
-
-namespace VisualAcademy.Infrastructures;
+﻿namespace VisualAcademy.Infrastructures;
 
 public class DefaultSchemaEnhancerCreateAllowedIPRangesTable(string defaultConnectionString)
 {
