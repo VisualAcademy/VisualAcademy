5dba1a8a9c0f8f8d4608e488192f32a95500662c
Park YongJune <redplus@redplus.net>
2024-06-27 00:26:20 +0900

    Constants class add

---
+++ src/VisualAcademy/VisualAcademy/Data/Constants.RoleNames.cs
@@ -0,0 +1,16 @@
+﻿namespace VisualAcademy.Data
+{
+    public static partial class Constants
+    {
+
+    }
+
+    public static partial class Constants
+    {
+        public static class RoleNames
+        {
+            public const string Administrators = "Administrators";
+            public const string Users = "Users";
+        }
+    }
+}
