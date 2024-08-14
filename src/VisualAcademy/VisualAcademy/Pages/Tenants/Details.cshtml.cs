10a92bfcaf3687546a10cf8f12a6a816bd0b5c66
Park YongJune <redplus@redplus.net>
2024-08-14 09:55:39 +0900

    Code cleanup

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Details.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Details.cshtml.cs
@@ -7,23 +7,28 @@ using System.Threading.Tasks;
 
 namespace VisualAcademy.Pages.Tenants;
 
-public class DetailsModel : PageModel {
+public class DetailsModel : PageModel
+{
     private readonly ApplicationDbContext _context;
 
-    public DetailsModel(ApplicationDbContext context) {
+    public DetailsModel(ApplicationDbContext context)
+    {
         _context = context;
     }
 
     public TenantModel TenantModel { get; set; }
 
-    public async Task<IActionResult> OnGetAsync(long? id) {
-        if (id == null) {
+    public async Task<IActionResult> OnGetAsync(long? id)
+    {
+        if (id == null)
+        {
             return NotFound();
         }
 
         TenantModel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
 
-        if (TenantModel == null) {
+        if (TenantModel == null)
+        {
             return NotFound();
         }
         return Page();
