ae881d6dadd0f600dc60846adae66718d3969ce4
Park YongJune <redplus@redplus.net>
2024-10-14 07:56:08 +0900

    TextMessages table add

---
+++ src/VisualAcademy/VisualAcademy.SqlServer/dbo/Tables/TextMessages.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TextMessages]
+(
+	[ID] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[EmployeeID] BIGINT NOT NULL,
+	[Message] NVARCHAR(MAX) NOT NULL,
+	[DateSent] DateTimeOffset(7) NOT NULL,
+	[TextMessageType] int null
+)
