11fbf3ff434c1a8b7b80122d86926c54fce7a101
Park YongJune <redplus@redplus.net>
2024-10-14 08:12:01 +0900

    TextMessage model class add

---
+++ src/VisualAcademy/VisualAcademy.Models/TextMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VisualAcademy.Models
+{
+    public class TextMessage
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public long EmployeeId { get; set; }
+
+        [Required]
+        public string Message { get; set; } = string.Empty;
+
+        public DateTimeOffset DateSent { get; set; } = DateTimeOffset.Now;
+
+        public int? TextMessageType { get; set; }
+    }
+}
