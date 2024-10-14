26a76b3ff971ccac51e5f51f835d9587c9816525
Park YongJune <redplus@redplus.net>
2024-10-15 05:39:22 +0900

    Remove unnecessary usings

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Edit.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Edit.cshtml.cs
@@ -1,50 +1,55 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using VisualAcademy.Data;
-using VisualAcademy.Models;
-
-namespace VisualAcademy.Pages.Tenants {
-    public class EditModel : PageModel {
+namespace VisualAcademy.Pages.Tenants
+{
+    public class EditModel : PageModel
+    {
         private readonly ApplicationDbContext _context;
 
-        public EditModel(ApplicationDbContext context) {
+        public EditModel(ApplicationDbContext context)
+        {
             _context = context;
         }
 
         [BindProperty]
         public TenantModel TenantModel { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(long? id) {
-            if (id == null) {
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
                 return NotFound();
             }
 
             TenantModel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
 
-            if (TenantModel == null) {
+            if (TenantModel == null)
+            {
                 return NotFound();
             }
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync() {
-            if (!ModelState.IsValid) {
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
                 return Page();
             }
 
             _context.Attach(TenantModel).State = EntityState.Modified;
 
-            try {
+            try
+            {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException) {
-                if (!TenantModelExists(TenantModel.Id)) {
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TenantModelExists(TenantModel.Id))
+                {
                     return NotFound();
                 }
-                else {
+                else
+                {
                     throw;
                 }
             }
@@ -52,7 +57,8 @@ namespace VisualAcademy.Pages.Tenants {
             return RedirectToPage("./Index");
         }
 
-        private bool TenantModelExists(long id) {
+        private bool TenantModelExists(long id)
+        {
             return _context.Tenants.Any(e => e.Id == id);
         }
     }
