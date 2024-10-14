810129592fd0489aa3badd0c65c51ebc76a2eb22
Park YongJune <redplus@redplus.net>
2024-10-15 05:42:28 +0900

    NullLogger add

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Edit.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Edit.cshtml.cs
@@ -3,10 +3,14 @@ namespace VisualAcademy.Pages.Tenants;
 public class EditModel : PageModel
 {
     private readonly ApplicationDbContext _context;
+    private readonly ILogger<EditModel> _logger;
 
-    public EditModel(ApplicationDbContext context)
+    public EditModel(ApplicationDbContext context, ILogger<EditModel> logger)
     {
         _context = context;
+
+        // 만약 logger가 null이면 NullLogger 인스턴스를 사용하여 예외 방지 
+        _logger = logger ?? Microsoft.Extensions.Logging.Abstractions.NullLogger<EditModel>.Instance;
     }
 
     [BindProperty]
@@ -14,6 +18,9 @@ public class EditModel : PageModel
 
     public async Task<IActionResult> OnGetAsync(long? id)
     {
+        // 로깅: GET 요청이 시작됨을 알림
+        _logger.LogInformation("OnGetAsync 메서드가 호출되었습니다."); 
+
         if (id == null)
         {
             return NotFound();
