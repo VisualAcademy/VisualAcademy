725225672214184c8fbb35ad5c1595504daebeae
Park YongJune <redplus@redplus.net>
2024-10-16 01:18:14 +0900

    TextTemplates table add

---
+++ src/VisualAcademy/VisualAcademy.SqlServer/dbo/Tables/TextTemplates.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TextTemplates]
+(
+    [ID] BIGINT NOT NULL PRIMARY KEY IDENTITY, -- 템플릿 아이디
+    [Message] NVARCHAR(MAX) NOT NULL, -- 템플릿 메시지
+    [Title] NVARCHAR(100) NULL -- 템플릿 제목
+)
