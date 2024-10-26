c44f507b5e1cdcde1a9ebc14a3239be99070b7bd
Park YongJune <redplus@redplus.net>
2024-10-26 23:29:33 +0900

    DogWalk class add

---
+++ src/VisualAcademy/VisualAcademy.Models/DogWalk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Models
+{
+    public class DogWalk
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public Terrain Terrain { get; set; }
+        public List<DateOnly> DaysVisited { get; private set; } = new();
+        public Pub? ClosestPub { get; set; }
+    }
+
+    public enum Terrain
+    {
+        Forest,
+        River,
+        Hills,
+        Village,
+        Park,
+        Beach,
+    }
+}
