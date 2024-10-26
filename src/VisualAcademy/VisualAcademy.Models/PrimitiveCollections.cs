710cf25ef9032603ac24a73eedc2131267e781dc
Park YongJune <redplus@redplus.net>
2024-10-26 23:28:41 +0900

    Pub class add

---
+++ src/VisualAcademy/VisualAcademy.Models/PrimitiveCollections.cs
@@ -0,0 +1,12 @@
+﻿namespace VisualAcademy.Models
+{
+    public class PrimitiveCollections
+    {
+        public IList<DateOnly> Dates { get; set; }
+        public uint[] UnsignedInts { get; set; }
+        public List<bool> Booleans { get; set; }
+        public List<Uri> Urls { get; set; }
+        public IEnumerable<int> Ints { get; set; } // Must be an IList<int>() at runtime.
+        public ICollection<string> Strings { get; set; } // Must be an IList<int>() at runtime.
+    }
+}
---
+++ src/VisualAcademy/VisualAcademy.Models/Pub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Models
+{
+    public class Pub
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string[] Beers { get; set; }
+        public List<DateOnly> DaysVisited { get; private set; } = new();
+    }
+}
