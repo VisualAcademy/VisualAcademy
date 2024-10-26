c5f6b267dcf201e64b63cb8a050bb6f0d6e63f21
Park YongJune <redplus@redplus.net>
2024-10-26 23:30:47 +0900

    PostSummary class add

---
+++ src/VisualAcademy/VisualAcademy.Models/PostSummary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Models
+{
+    public class PostSummary
+    {
+        public string BlogName { get; set; }
+        public string PostTitle { get; set; }
+        public DateOnly PublishedOn { get; set; }
+    }
+}
