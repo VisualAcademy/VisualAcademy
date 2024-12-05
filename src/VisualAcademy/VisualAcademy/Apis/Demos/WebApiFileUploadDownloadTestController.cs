5b566a6372e8f1d8a5e64eab9fdc13049dee1a9d
Park YongJune <redplus@redplus.net>
2024-12-05 14:17:30 +0900

    WebApiFileUploadDownloadTestController initial add

---
+++ src/VisualAcademy/VisualAcademy/Apis/Demos/WebApiFileUploadDownloadTestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Apis.Demos
+{
+    public class WebApiFileUploadDownloadTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
