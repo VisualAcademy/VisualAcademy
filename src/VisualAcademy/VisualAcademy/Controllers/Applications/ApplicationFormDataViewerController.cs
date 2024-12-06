f33423668f98b642359f2c207bc17a3e73b0a840
Park YongJune <redplus@redplus.net>
2024-12-06 15:57:56 +0900

    Remove unnecessary usings

--- src/VisualAcademy/VisualAcademy/Controllers/Applications/ApplicationFormDataViewerController.cs
+++ src/VisualAcademy/VisualAcademy/Controllers/Applications/ApplicationFormDataViewerController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace VisualAcademy.Controllers.Applications
+﻿namespace VisualAcademy.Controllers.Applications
 {
     public class ApplicationFormDataViewerController : Controller
     {
