27b2a8dd9bf4eced7d1bb7a50dec95bfa7e64d32
Park YongJune <redplus@redplus.net>
2024-12-06 15:58:42 +0900

    Use expression body for method

--- src/VisualAcademy/VisualAcademy/Controllers/Applications/ApplicationFormDataViewerController.cs
+++ src/VisualAcademy/VisualAcademy/Controllers/Applications/ApplicationFormDataViewerController.cs
@@ -2,8 +2,5 @@
 
 public class ApplicationFormDataViewerController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
+    public IActionResult Index() => View();
 }
