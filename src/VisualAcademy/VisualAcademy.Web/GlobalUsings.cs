465bd864e4250702390652f1a9b0d83a23aa3325
Park YongJune <redplus@redplus.net>
2024-12-23 19:10:08 +0900

    GlobalUsings.cs file add

---
+++ src/VisualAcademy/VisualAcademy.Web/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.UI.Services;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Diagnostics;
+global using System.Security.Claims;
+global using System.Text;
+global using System.Text.Encodings.Web;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using System.Text.Json;
