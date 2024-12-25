421e59b3462f34098a02baa25fd926c0f90d2de0
Park YongJune <redplus@redplus.net>
2024-12-25 19:49:41 +0900

    EmailSender Test Class add

---
+++ src/VisualAcademy/VisualAcademy/Services/EmailSender.cs
@@ -0,0 +1,12 @@
+﻿
+namespace VisualAcademy.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            Console.WriteLine(htmlMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
