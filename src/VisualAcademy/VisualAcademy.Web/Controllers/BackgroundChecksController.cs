326d103e6891c29518ba24865ded3e53a7fd7978
Park YongJune <redplus@redplus.net>
2025-01-14 06:34:25 +0900

    BackgroundChecksController controller class file add

---
+++ src/VisualAcademy/VisualAcademy.Web/Controllers/BackgroundChecksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Web.Controllers
+{
+    public class BackgroundChecksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
