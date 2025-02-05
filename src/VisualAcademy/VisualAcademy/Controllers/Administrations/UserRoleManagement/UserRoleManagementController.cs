a3a15ae881bbf98559d11854728de2e10209c699
Park YongJune <redplus@redplus.net>
2025-02-05 13:06:00 +0900

    UserRoleManagementController controller class file add.

---
+++ src/VisualAcademy/VisualAcademy/Controllers/Administrations/UserRoleManagement/UserRoleManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Controllers.Administrations.UserRoleManagement
+{
+    public class UserRoleManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
