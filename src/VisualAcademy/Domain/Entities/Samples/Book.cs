6a0bb9e5092ceface5f3270439c96e807a98f77a
Park YongJune <redplus@redplus.net>
2025-02-20 20:24:09 +0900

    Book class file add.

---
+++ src/VisualAcademy/Domain/Entities/Samples/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities.Samples
+{
+    //[EntityTypeConfiguration(typeof(BookConfiguration))]
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+    }
+}
