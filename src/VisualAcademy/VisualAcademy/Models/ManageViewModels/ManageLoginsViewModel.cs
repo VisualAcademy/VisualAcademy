94b1ce2cd540b1cdd46997e3ded4aedfbfe68281
Park YongJune <redplus@redplus.net>
2025-02-22 13:03:19 +0900

    .

---
+++ src/VisualAcademy/VisualAcademy/Models/ManageViewModels/ManageLoginsViewModel.cs
@@ -0,0 +1,9 @@
+﻿//namespace VisualAcademy.Models.ManageViewModels
+//{
+//    public class ManageLoginsViewModel
+//    {
+//        public IList<UserLoginInfo> CurrentLogins { get; set; }
+
+//        public IList<AuthenticationDescription> OtherLogins { get; set; }
+//    }
+//}
