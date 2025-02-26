e598689b5792ac6d14812ce2d877e0a46c3753b4
Park YongJune <redplus@redplus.net>
2025-02-27 05:01:19 +0900

    .

--- 8.0/IPRanges/blazor/VisualAcademy/VisualAcademy/Models/AllowedIPRange.cs
+++ 8.0/IPRanges/blazor/VisualAcademy/VisualAcademy/Models/AllowedIPRange.cs
@@ -7,6 +7,6 @@ public class AllowedIPRange
     public string StartIPRange { get; set; }
     public string EndIPRange { get; set; }
     public string Description { get; set; }
-    public DateTime CreateDate { get; set; }
+    public DateTime CreateDate { get; set; } = DateTime.Now; // 기본값 설정
     public long TenantId { get; set; }
 }
