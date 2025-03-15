5391d9a248ba8b195445155d3e52e27bdf591959
Park YongJune <redplus@redplus.net>
2025-03-15 09:20:20 +0900

    ArticlesController class file added.

---
+++ src/VisualAcademy/VisualAcademy/Controllers/ArticlesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
