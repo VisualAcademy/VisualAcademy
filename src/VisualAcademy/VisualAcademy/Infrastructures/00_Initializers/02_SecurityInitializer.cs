af4f48caccb91fcaff795477e0da0000a80dd3f5
Park YongJune <redplus@redplus.net>
2025-06-19 10:44:26 +0900

    Update InitializeRulesTable method

--- src/VisualAcademy/VisualAcademy/Infrastructures/00_Initializers/02_SecurityInitializer.cs
+++ src/VisualAcademy/VisualAcademy/Infrastructures/00_Initializers/02_SecurityInitializer.cs
@@ -30,7 +30,7 @@ public static class SecurityInitializer
                 ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
             // appName을 구분하여 시드
-            ResourceSeeder.InsertRequiredResources(connectionString, logger, appName: null); // 전체
+            //ResourceSeeder.InsertRequiredResources(connectionString, logger, appName: null); // 전체
             ResourceSeeder.InsertRequiredResources(connectionString, logger, appName: "VisualAcademy");
 
             logger.LogInformation($"{target}의 Resources 시드 데이터 삽입 완료");
@@ -48,6 +48,14 @@ public static class SecurityInitializer
         {
             Azunt.RuleManagement.RulesTableBuilder.Run(services, forMaster);
             logger.LogInformation($"{target}의 Rules 테이블 초기화 완료");
+
+            // 시드 데이터 삽입 추가
+            var config = services.GetRequiredService<IConfiguration>();
+            var connectionString = config.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            Azunt.RuleManagement.RuleSeeder.SeedAdministratorRoleRules(connectionString, logger);
+            logger.LogInformation($"{target}의 Rules 시드 데이터 삽입 완료");
         }
         catch (Exception ex)
         {
