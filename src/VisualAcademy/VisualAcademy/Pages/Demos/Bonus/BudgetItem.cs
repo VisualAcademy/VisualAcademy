3b560df2cdee16ae22c3b4596ca9808e7cda11ae
Park YongJune <redplus@redplus.net>
2025-09-28 23:06:07 +0900

    .

--- src/VisualAcademy/VisualAcademy/Pages/Demos/Bonus/BudgetItem.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Demos/Bonus/BudgetItem.cs
@@ -2,7 +2,7 @@
 {
     public class BudgetItem
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public decimal Amount { get; set; }
     }
 }
