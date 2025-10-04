b843740b839ec54f0a971e252aad6c7f2c6bb014
Park YongJune <redplus@redplus.net>
2025-10-04 23:00:37 +0900

    Create and run an ASP.NET Core MVC project with .NET 10 under the VisualAcademy name

---
+++ 10.0/MVC/VisualAcademy/VisualAcademy/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VisualAcademy.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
