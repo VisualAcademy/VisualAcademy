55ac4a8795fbaab821bb78866a0a53fb27883992
Park YongJune <redplus@redplus.net>
2025-10-29 13:45:38 +0900

    InvestigationsReportsController

---
+++ src/VisualAcademy/VisualAcademy/Controllers/InvestigationsReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Controllers
+{
+    public class InvestigationsReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
