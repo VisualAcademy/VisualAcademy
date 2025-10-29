e99a849597d092f6ac601c247a6a5cb3e265bda1
Park YongJune <redplus@redplus.net>
2025-10-30 07:04:39 +0900

    ASP.NET Core MVC 10.0 기본 템플릿으로 프로젝트 만들고 실행하기

---
+++ 10.0/MVC/Azunt.Web/Azunt.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Azunt.Web.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
