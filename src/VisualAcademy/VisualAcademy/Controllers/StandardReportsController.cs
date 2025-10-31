402b0140a8a38b0445364fa25513171c8139f1ba
Park YongJune <redplus@redplus.net>
2025-11-01 05:00:30 +0900

    Add Standard Reports controller and view

---
+++ src/VisualAcademy/VisualAcademy/Controllers/StandardReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Controllers
+{
+    public class StandardReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
