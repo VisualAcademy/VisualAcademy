17bb5166556f60db65268f4ca39099e18a7375f9
Park YongJune <redplus@redplus.net>
2026-01-10 22:46:29 +0900

    Add required modifier to TodoItem.Title

--- src/VisualAcademy/VisualAcademy.Models/TodoItem.cs
+++ src/VisualAcademy/VisualAcademy.Models/TodoItem.cs
@@ -3,6 +3,6 @@
 public class TodoItem
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public required string Title { get; set; }
     public bool IsDone { get; set; }
 }
