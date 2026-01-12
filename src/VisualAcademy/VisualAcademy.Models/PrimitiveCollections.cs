c001c224177a901489ebcd5d269d94b239fe618d
Park YongJune <redplus@redplus.net>
2026-01-13 04:15:15 +0900

    Initialize PrimitiveCollections properties with empty collections

--- src/VisualAcademy/VisualAcademy.Models/PrimitiveCollections.cs
+++ src/VisualAcademy/VisualAcademy.Models/PrimitiveCollections.cs
@@ -2,11 +2,11 @@
 {
     public class PrimitiveCollections
     {
-        public IList<DateOnly> Dates { get; set; }
-        public uint[] UnsignedInts { get; set; }
-        public List<bool> Booleans { get; set; }
-        public List<Uri> Urls { get; set; }
-        public IEnumerable<int> Ints { get; set; } // Must be an IList<int>() at runtime.
-        public ICollection<string> Strings { get; set; } // Must be an IList<int>() at runtime.
+        public IList<DateOnly> Dates { get; set; } = new List<DateOnly>();
+        public uint[] UnsignedInts { get; set; } = Array.Empty<uint>();
+        public List<bool> Booleans { get; set; } = new();
+        public List<Uri> Urls { get; set; } = new();
+        public IEnumerable<int> Ints { get; set; } = new List<int>();
+        public ICollection<string> Strings { get; set; } = new List<string>();
     }
 }
