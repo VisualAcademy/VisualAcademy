029dfa22d5153bf9af865a87ea4148696aaeed5b
Park YongJune <redplus@redplus.net>
2026-01-14 04:02:58 +0900

    Mark Book properties as required to fix CS8618 warnings

--- src/VisualAcademy/Domain/Entities/Samples/Book.cs
+++ src/VisualAcademy/Domain/Entities/Samples/Book.cs
@@ -4,7 +4,7 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Isbn { get; set; }
+        public required string Title { get; set; }
+        public required string Isbn { get; set; }
     }
 }
