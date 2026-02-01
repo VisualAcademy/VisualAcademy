10e8ab325c582d9219ca02c306cd9a90aa0ab6de
Park YongJune <redplus@redplus.net>
2026-02-02 01:35:46 +0900

    Fix CS8618 warning in AppointmentsTypes IndexModel by initializing AppointmentTypes collection

--- src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Index.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Index.cshtml.cs
@@ -2,15 +2,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models.Tenants;
 using VisualAcademy.Repositories.Tenants;
 
-namespace VisualAcademy.Pages.AppointmentsTypes; 
-public class IndexModel : PageModel {
+namespace VisualAcademy.Pages.AppointmentsTypes;
+
+public class IndexModel : PageModel
+{
     private readonly IAppointmentTypeRepository _appointmentTypeRepository;
 
-    public IndexModel(IAppointmentTypeRepository appointmentTypeRepository) => _appointmentTypeRepository = appointmentTypeRepository;
+    public IndexModel(IAppointmentTypeRepository appointmentTypeRepository)
+        => _appointmentTypeRepository = appointmentTypeRepository;
 
-    public IList<AppointmentTypeModel> AppointmentTypes { get; set; }
+    public IList<AppointmentTypeModel> AppointmentTypes { get; set; } = new List<AppointmentTypeModel>();
 
-    public async Task OnGetAsync() {
+    public async Task OnGetAsync()
+    {
         long tenantId = 1; // tenantId 값을 1로 초기화합니다.
         AppointmentTypes = await _appointmentTypeRepository.GetAllAsync(tenantId);
     }
