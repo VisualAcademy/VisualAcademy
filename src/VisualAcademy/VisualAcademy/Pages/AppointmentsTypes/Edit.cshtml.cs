ce3ba7a0f6ea249fb85903bbc9f31da860ae3e3e
Park YongJune <redplus@redplus.net>
2026-03-02 15:35:58 +0900

    .

--- src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Edit.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Edit.cshtml.cs
@@ -1,8 +1,10 @@
 using VisualAcademy.Models.Tenants;
 using VisualAcademy.Repositories.Tenants;
 
-namespace VisualAcademy.Pages.AppointmentsTypes {
-    public class EditModel : PageModel {
+namespace VisualAcademy.Pages.AppointmentsTypes
+{
+    public class EditModel : PageModel
+    {
         private readonly IAppointmentTypeRepository _appointmentTypeRepository;
 
         public EditModel(IAppointmentTypeRepository appointmentTypeRepository) => _appointmentTypeRepository = appointmentTypeRepository;
@@ -10,23 +12,27 @@ namespace VisualAcademy.Pages.AppointmentsTypes {
         [BindProperty]
         public AppointmentTypeModel AppointmentType { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(long id) {
+        public async Task<IActionResult> OnGetAsync(long id)
+        {
             long tenantId = 1; // tenantId 값을 1로 초기화합니다.
             AppointmentType = await _appointmentTypeRepository.GetByIdAsync(id, tenantId);
 
-            if (AppointmentType == null) {
+            if (AppointmentType == null)
+            {
                 return NotFound();
             }
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync() {
-            if (!ModelState.IsValid) {
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
                 return Page();
             }
 
             long tenantId = 1; // tenantId 값을 1로 초기화합니다.
-            AppointmentType.TenantId = tenantId; 
+            AppointmentType.TenantId = tenantId;
             await _appointmentTypeRepository.UpdateAsync(AppointmentType);
 
             return RedirectToPage("./Index");
