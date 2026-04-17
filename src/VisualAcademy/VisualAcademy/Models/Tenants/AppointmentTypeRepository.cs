d81c1cbacfa44b64b4eda32b9b925d36a7fbe1d5
Park YongJune <redplus@redplus.net>
2026-04-18 01:47:41 +0900

    update appointment type repository interface to allow nullable get-by-id result

--- src/VisualAcademy/VisualAcademy/Models/Tenants/AppointmentTypeRepository.cs
+++ src/VisualAcademy/VisualAcademy/Models/Tenants/AppointmentTypeRepository.cs
@@ -1,35 +1,47 @@
-﻿using VisualAcademy.Models.Tenants;
+﻿using Microsoft.EntityFrameworkCore;
+using VisualAcademy.Models.Tenants;
 
 namespace VisualAcademy.Repositories.Tenants;
 
-public class AppointmentTypeRepository : IAppointmentTypeRepository {
+public class AppointmentTypeRepository : IAppointmentTypeRepository
+{
     private readonly ApplicationDbContext _dbContext;
 
-    public AppointmentTypeRepository(ApplicationDbContext dbContext) {
+    public AppointmentTypeRepository(ApplicationDbContext dbContext)
+    {
         _dbContext = dbContext;
     }
 
-    public async Task<List<AppointmentTypeModel>> GetAllAsync(long tenantId) {
-        return await _dbContext.AppointmentsTypes.Where(a => a.TenantId == tenantId).ToListAsync();
+    public async Task<List<AppointmentTypeModel>> GetAllAsync(long tenantId)
+    {
+        return await _dbContext.AppointmentsTypes
+            .Where(a => a.TenantId == tenantId)
+            .ToListAsync();
     }
 
-    public async Task<AppointmentTypeModel> GetByIdAsync(long id, long tenantId) {
-        return await _dbContext.AppointmentsTypes.Where(a => a.Id == id && a.TenantId == tenantId).FirstOrDefaultAsync();
+    public async Task<AppointmentTypeModel?> GetByIdAsync(long id, long tenantId)
+    {
+        return await _dbContext.AppointmentsTypes
+            .FirstOrDefaultAsync(a => a.Id == id && a.TenantId == tenantId);
     }
 
-    public async Task AddAsync(AppointmentTypeModel appointmentType) {
+    public async Task AddAsync(AppointmentTypeModel appointmentType)
+    {
         _dbContext.Add(appointmentType);
         await _dbContext.SaveChangesAsync();
     }
 
-    public async Task UpdateAsync(AppointmentTypeModel appointmentType) {
+    public async Task UpdateAsync(AppointmentTypeModel appointmentType)
+    {
         _dbContext.Entry(appointmentType).State = EntityState.Modified;
         await _dbContext.SaveChangesAsync();
     }
 
-    public async Task DeleteAsync(long id, long tenantId) {
+    public async Task DeleteAsync(long id, long tenantId)
+    {
         var appointmentType = await GetByIdAsync(id, tenantId);
-        if (appointmentType != null) {
+        if (appointmentType is not null)
+        {
             _dbContext.Remove(appointmentType);
             await _dbContext.SaveChangesAsync();
         }
--- src/VisualAcademy/VisualAcademy/Models/Tenants/IAppointmentTypeRepository.cs
+++ src/VisualAcademy/VisualAcademy/Models/Tenants/IAppointmentTypeRepository.cs
@@ -1,9 +1,11 @@
 ﻿using VisualAcademy.Models.Tenants;
 
-namespace VisualAcademy.Repositories.Tenants {
-    public interface IAppointmentTypeRepository {
+namespace VisualAcademy.Repositories.Tenants
+{
+    public interface IAppointmentTypeRepository
+    {
         Task<List<AppointmentTypeModel>> GetAllAsync(long tenantId);
-        Task<AppointmentTypeModel> GetByIdAsync(long id, long tenantId);
+        Task<AppointmentTypeModel?> GetByIdAsync(long id, long tenantId);
         Task AddAsync(AppointmentTypeModel appointmentType);
         Task UpdateAsync(AppointmentTypeModel appointmentType);
         Task DeleteAsync(long id, long tenantId);
