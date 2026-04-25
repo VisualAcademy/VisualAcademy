991cd1060f4775ddbe01a0bbf7706d5556a8a1b4
Park YongJune <redplus@redplus.net>
2026-04-25 15:07:45 +0900

    Update profile picture upload page to Bootstrap 5 and add it to the account management navigation

--- src/VisualAcademy/VisualAcademy/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ src/VisualAcademy/VisualAcademy/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -60,6 +60,8 @@ public static class ManageNavPages
     /// </summary>
     public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+    public static string ProfilePictureUpload => "ProfilePictureUpload";
+
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
@@ -107,6 +109,9 @@ public static class ManageNavPages
     ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
     public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+
+    public static string ProfilePictureUploadNavClass(ViewContext viewContext)
+        => PageNavClass(viewContext, ProfilePictureUpload);
 
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
