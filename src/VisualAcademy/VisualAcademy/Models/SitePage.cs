b8011ca76cbe36c6f00f84904ce6950f7c07d629
Park YongJune <redplus@redplus.net>
2026-05-01 05:18:06 +0900

    fix model class

--- src/VisualAcademy/VisualAcademy/Models/SitePage.cs
+++ src/VisualAcademy/VisualAcademy/Models/SitePage.cs
@@ -1,7 +1,14 @@
-﻿public class SitePage
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Azunt.Web.Models;
+
+public class SitePage
 {
     public long Id { get; set; }
 
+    [Required]
+    [MaxLength(300)]
     public string RoutePattern { get; set; } = "";
 
     public string? HttpMethod { get; set; }
@@ -14,9 +21,9 @@
 
     public int SortOrder { get; set; }
 
-    public bool IsPublic { get; set; }
+    public bool IsPublic { get; set; } = true;
 
-    public bool IsVisibleInDashboard { get; set; }
+    public bool IsVisibleInDashboard { get; set; } = true;
 
     public string? RequiredRoles { get; set; }
 
@@ -24,7 +31,25 @@
 
     public bool AllowAnonymous { get; set; }
 
-    public bool IsEndpointActive { get; set; }
+    public bool IsEndpointActive { get; set; } = true;
 
-    public string Url => RoutePattern == "/" ? "/" : "/" + RoutePattern.TrimStart('/');
+    public DateTime? LastSyncedAtUtc { get; set; }
+
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAtUtc { get; set; }
+
+    [NotMapped]
+    public string Url
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(RoutePattern))
+                return "/";
+
+            var route = RoutePattern.Trim();
+
+            return route.StartsWith("/") ? route : "/" + route;
+        }
+    }
 }
