8a3cced88a7c39bfbe70f3998889902822957228
Park YongJune <redplus@redplus.net>
2026-05-01 04:59:54 +0900

    Add SitePagesController Controller class file

---
+++ src/VisualAcademy/VisualAcademy/Controllers/SitePagesController.cs
@@ -0,0 +1,34 @@
+﻿using Azunt.Web.Data;
+using Azunt.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Azunt.Web.Controllers;
+
+public class SitePagesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    private readonly SitePageRouteSyncService _sync;
+
+    public SitePagesController(
+        ApplicationDbContext context,
+        SitePageRouteSyncService sync)
+    {
+        _context = context;
+        _sync = sync;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.SitePages
+            .OrderBy(x => x.SortOrder)
+            .ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Sync()
+    {
+        await _sync.SyncAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
