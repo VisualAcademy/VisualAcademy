3c1fc8ac4a0bc1d10c4bf99dd497401eef423955
Park YongJune <redplus@redplus.net>
2026-04-30 14:41:35 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy.Models/PrimitiveCollections.cs
+++ src/VisualAcademy/VisualAcademy.Models/PrimitiveCollections.cs
@@ -1,12 +1,11 @@
-﻿namespace VisualAcademy.Models
+﻿namespace VisualAcademy.Models;
+
+public class PrimitiveCollections
 {
-    public class PrimitiveCollections
-    {
-        public IList<DateOnly> Dates { get; set; } = new List<DateOnly>();
-        public uint[] UnsignedInts { get; set; } = Array.Empty<uint>();
-        public List<bool> Booleans { get; set; } = new();
-        public List<Uri> Urls { get; set; } = new();
-        public IEnumerable<int> Ints { get; set; } = new List<int>();
-        public ICollection<string> Strings { get; set; } = new List<string>();
-    }
+    public IList<DateOnly> Dates { get; set; } = new List<DateOnly>();
+    public uint[] UnsignedInts { get; set; } = Array.Empty<uint>();
+    public List<bool> Booleans { get; set; } = new();
+    public List<Uri> Urls { get; set; } = new();
+    public IEnumerable<int> Ints { get; set; } = new List<int>();
+    public ICollection<string> Strings { get; set; } = new List<string>();
 }
