7c1c14e6a081e7ac77e37b76afa37d3e026f00d7
Park YongJune <redplus@redplus.net>
2026-06-02 04:06:37 +0900

    Add Sessions controller and view

---
+++ src/VisualAcademy/VisualAcademy/Controllers/SessionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
