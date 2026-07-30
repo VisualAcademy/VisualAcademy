1875e9f923e8438fb0900897679f0eb2d8e51f2b
Park YongJune <redplus@redplus.net>
2026-07-30 15:36:16 +0900

    Remove unnecessary usings

--- src/VisualAcademy/VisualAcademy/Initializers/AttachmentTenantSchemaRunner.cs
+++ src/VisualAcademy/VisualAcademy/Initializers/AttachmentTenantSchemaRunner.cs
@@ -1,8 +1,4 @@
 ﻿using Azunt.AttachmentManagement;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 
 namespace Azunt.Initializers;
 
